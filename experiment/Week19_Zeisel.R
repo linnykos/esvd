02251f890ce109d3e9d7b10e7df5eb158a8850ec
klin1234@gmail.com <klin1234@gmail.com>
2018-08-29 09:18:15 -0400

    Wrapping up the writeup for 19

--- experiment/Week19_Zeisel.R
+++ experiment/Week19_Zeisel.R
@@ -29,7 +29,8 @@ diff_vec <- sapply(1:ncol(dat_subset), function(i){
   }
 })
 
-hist_augment <- function(x, breaks = 50, min_val = log10(1.01), ...){
+hist_augment <- function(x, breaks = 50, min_val = log10(1.01),
+                         multiplier = 1, param = NA, lwd = 1, ...){
   break_vec <- seq(min_val, max(x), length.out = breaks)
 
   min_nonzero <- min(x[x != min_val])
@@ -42,10 +43,41 @@ hist_augment <- function(x, breaks = 50, min_val = log10(1.01), ...){
   break_vec <- break_vec + interval/2
   break_vec <- c(break_vec[1] - diff(break_vec)[1], break_vec)
 
-  zz <- hist(x, breaks = break_vec, ...)
+  zz <- hist(x, breaks = break_vec, col = "gray", ...)
   len <- length(which(x == min_val))
   if(len != 0){
     rect(zz$breaks[1], 0, zz$breaks[2], len, col = rgb(0.803, 0.156, 0.211))
+  }
+
+  if(!all(is.na(param))){
+    x_seq <- seq(min_val, max(x), length.out = 1000)
+
+   if(class(param) == "Gamma-Normal" | class(param) == "Gamma-TNormal"){
+      gamma_dist <- param[1] * sapply(x_seq, stats::dgamma, shape = param[2], rate = param[3])
+      norm_dist <- (1-param[1]) * sapply(x_seq, stats::dnorm, mean = param[4], sd = param[5])
+      if(class(param) == "Gamma-TNormal") norm_dist <- norm_dist / (1 - stats::pnorm(0, mean = param[4], sd = param[5]))
+
+      density_list <- list(multiplier*gamma_dist, multiplier*norm_dist)
+    } else {
+      norm_dist <- (1-param[1]) * sapply(x_seq, stats::dnorm, mean = param[3], sd = param[4])
+      density_list <- list(NA, multiplier*norm_dist)
+    }
+
+    ymax <- max(unlist(density_list), na.rm = T)
+    print(ymax)
+
+    if(!any(is.na(density_list[[1]]))) {
+      legend("topright", c("Gaussian distribution","Gamma distribution"),
+             bty="n", fill=c(rgb(0.584, 0.858, 0.564), rgb(0.803, 0.156, 0.211)));
+
+      lines(x_seq, density_list[[1]], col = rgb(0.803, 0.156, 0.211), lwd = lwd, lty = 2, ... )
+    } else {
+      legend("topright", c("Gaussian distribution","Delta distribution"),
+             bty="n", fill=c(rgb(0.584, 0.858, 0.564), rgb(0.803, 0.156, 0.211)));
+
+      lines(rep(log10(1.01), 2), c(-1e6, 1e6), col = rgb(0.803, 0.156, 0.211), lwd = lwd, lty = 2, ... )
+    }
+    lines(x_seq, density_list[[2]], col = rgb(0.584, 0.858, 0.564), lwd = lwd, lty = 2, ...)
   }
 
   invisible()
@@ -148,14 +180,6 @@ table(class1, class2)
 #enumerate over all genes and see if there is EVER a instance where
 # log10(1.01) is classified as "true"
 
-param_list <- lapply(1:ncol(dat_subset), function(x){
-  print(x)
-
-  vec <- dat_subset[,x]
-  if(length(vec[vec > log10(1.01)]) < 5) return(NA)
-
-  get_mix_switch(vec, truncated = T)
-})
 
 cutoff_vec <- sapply(1:ncol(dat_subset), function(x){
   if(x %% floor(ncol(dat_subset)/10) == 0) cat('*')
@@ -172,6 +196,8 @@ cutoff_vec <- sapply(1:ncol(dat_subset), function(x){
   }
 })
 length(which(cutoff_vec == log10(1.01)))
+idx <- which(cutoff_vec == log10(1.01))[1]
+
 
 class_vec <- sapply(1:ncol(dat_subset), function(x){
   if(x %% floor(ncol(dat_subset)/10) == 0) cat('*')
@@ -188,10 +214,72 @@ count_vec <- sapply(1:ncol(dat_subset), function(x){
   vec <- dat_subset[,x]
   length(vec[vec != log10(1.01)])
 })
+length(which(count_vec < 10))
 
-cutoff_vec2 <- cutoff_vec[intersect(which(!is.na(cutoff_vec)),
-                                    which(!is.infinite(cutoff_vec)))]
+compact_vec <- sapply(1:ncol(dat_subset), function(x){
+  if(x %% floor(ncol(dat_subset)/10) == 0) cat('*')
+
+  vec <- dat_subset[,x]
+  if(length(vec[vec > log10(1.01)]) < 5) return(NA)
+
+  res <- get_mix_switch(vec)
+  if(class(res) == "Delta-Gaussian") return(NA)
+  class_vec <- compute_dropout(vec, res)
+  idx <- which(vec == log10(1.01))
+  if(length(idx) == 0) return(NA)
+  length(which(class_vec[idx] == 2))/length(which(class_vec == 2))
+})
+which(compact_vec == max(compact_vec, na.rm = T))
+
+###
+# gaussian and gamma switching roles
+
+vec <- dat_subset[,17]
+res <- get_mix_switch(vec)
+table(compute_dropout(vec, res))
+png("../figure/experiment/19_zeisel_hist1.png", height = 1200, width = 1500, res = 300, units = "px")
+hist_augment(vec, breaks = 100, param = res, lwd = 3, multiplier = 5,
+             xlab = "Value", ylab = "Frequency", main = "")
+graphics.off()
+
+#####
+# gaussian pulled out
+
+vec <- dat_subset[,340]
+res <- get_mix_switch(vec)
+table(compute_dropout(vec, res))
+png("../figure/experiment/19_zeisel_hist2.png", height = 1200, width = 1500, res = 300, units = "px")
+hist_augment(vec, breaks = 100, param = res,lwd = 3, multiplier = 10,
+             xlab = "Value", ylab = "Frequency", main = "")
+graphics.off()
+
+# #########
+vec <- dat_subset[,1900]
+res1 <- get_mix(vec); class(res1) <- "Gamma-Normal"
+res2 <- get_mix_switch(vec)
+png("../figure/experiment/19_zeisel_hist3.png", height = 1200, width = 2500, res = 300, units = "px")
+par(mfrow = c(1,2))
+hist_augment(vec, breaks = 100, param = res1, lwd = 3, multiplier = 1,
+             xlab = "Value", ylab = "Frequency", main = "Gamma-Gaussian model")
+hist_augment(vec, breaks = 100, param = res2,lwd = 3, multiplier = 1,
+             xlab = "Value", ylab = "Frequency", main = "Delta-Gaussian model")
+graphics.off()
 
 
+#
+# cutoff_vec2 <- cutoff_vec[intersect(which(!is.na(cutoff_vec)),
+#                                     which(!is.infinite(cutoff_vec)))]
 
 
+##############################
+
+# using truncated gaussian
+
+param_list <- lapply(1:ncol(dat_subset), function(x){
+  print(x)
+
+  vec <- dat_subset[,x]
+  if(length(vec[vec > log10(1.01)]) < 5) return(NA)
+
+  get_mix_switch(vec, truncated = T)
+})
