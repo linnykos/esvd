d43ba7f6f9045141cb09b3ba54830663c64c3d7e
klin1234@gmail.com <klin1234@gmail.com>
2018-08-29 10:29:51 -0400

    A failed version of truncated normal..

---
+++ experiment/truncated_normal_mle.R
@@ -0,0 +1,178 @@
+# https://github.com/cran/tmvtnorm/blob/master/R/tmvnorm-estimation.R
+
+# estimation methods for the parameters of the truncated multivariate normal distribution
+#
+# Literatur:
+#
+# Amemiya (1974)   : Instrumental Variables estimator
+# Lee (1979)
+# Lee (1983)
+# Griffiths (2002) :
+# "Gibbs Sampler for the parameters of the truncated multivariate normal distribution"
+#
+# Stefan Wilhelm, wilhelm@financial.com
+#library(tmvtnorm)
+library(stats4)
+
+# Hilfsfunktion : VECH() Operator
+vech=function (x)
+{
+  # PURPOSE: creates a column vector by stacking columns of x
+  #          on and below the diagonal
+  #----------------------------------------------------------
+  # USAGE:  v = vech(x)
+  # where:  x = an input matrix
+  #---------------------------------------------------------
+  # RETURNS:
+  #         v = output vector containing stacked columns of x
+  #----------------------------------------------------------
+
+  # Written by Mike Cliff, UNC Finance  mcliff@unc.edu
+  # CREATED: 12/08/98
+
+  #if(!is.matrix(x))
+  #{
+  #
+  #}
+
+  rows    = nrow(x)
+  columns = ncol(x);
+  v = c();
+  for (i in 1:columns)
+  {
+    v = c(v, x[i:rows,i]);
+  }
+  v
+}
+
+# Hilfsfunktion : Operator für Namensgebung sigma_i.j (i <= j), d.h. wie vech(), nur Zeilenweise
+vech2 <- function (x)
+{
+  # PURPOSE: creates a column vector by stacking columns of x
+  #          on and below the diagonal
+  #----------------------------------------------------------
+  # USAGE:  v = vech2(x)
+  # where:  x = an input matrix
+  #---------------------------------------------------------
+  # RETURNS:
+  #         v = output vector containing stacked columns of x
+  #----------------------------------------------------------
+
+  # Written by Mike Cliff, UNC Finance  mcliff@unc.edu
+  # CREATED: 12/08/98
+
+  rows    = nrow(x)
+  columns = ncol(x);
+  v = c();
+  for (i in 1:rows)
+  {
+    v = c(v, x[i,i:columns]);
+  }
+  v
+}
+
+# Hilfsfunktion : Inverser VECH() Operator
+inv_vech=function(v)
+{
+  #----------------------------------------------------------
+  # USAGE:  x = inv_vech(v)
+  # where:  v = a vector
+  #---------------------------------------------------------
+  # RETURNS:
+  #         x = a symmetric (m x m) matrix containing de-vectorized elements of v
+  #----------------------------------------------------------
+
+  # Anzahl der Zeilen
+  m = -0.5+sqrt(0.5^2+2*length(v))
+  x = matrix(0,nrow=m,ncol=m)
+  if (length(v) != m*(m+1)/2)
+  {
+    # error
+    stop("v must have m*(m+1)/2 elements")
+  }
+
+  for (i in 1:m)
+  {
+    #cat("r=",i:m," c=",i,"\n")
+    x[ i:m, i]   = v[((i-1)*(m-(i-2)*0.5)+1) : (i*(m-(i-1)*0.5))]
+    x[ i,   i:m] = v[((i-1)*(m-(i-2)*0.5)+1) : (i*(m-(i-1)*0.5))]
+  }
+  x
+}
+
+# 1. Maximum-Likelihood-Estimation of mu and sigma when truncation points are known
+#
+# TODO/Idee: Cholesky-Zerlegung der Kovarianzmatrix als Parametrisierung
+#
+# @param X data matrix (T x n)
+# @param lower, upper truncation points
+# @param start list of start values for mu and sigma
+# @param fixed a list of fixed parameters
+# @param method
+# @param cholesky flag, if TRUE, we use the Cholesky decomposition of sigma as parametrization
+# @param lower.bounds lower bounds for method "L-BFGS-B"
+# @param upper.bounds upper bounds for method "L-BFGS-B"
+mle.tmvnorm <- function(X, weights = rep(1, length(X)),
+                        lower=0,
+                        upper=+Inf,
+                        start=list(mu=mean(X), sigma=sd(X)),
+                        fixed=list(), method="L-BFGS-B",
+                        cholesky=FALSE,
+                        lower.bounds=c(0, 1e-5),
+                        upper.bounds=c(+Inf, +Inf),
+                        ...)
+{
+  # verify dimensions of x and lower/upper match
+  n <- length(X)
+
+  if ((length(lower.bounds) > 1L || length(upper.bounds) > 1L || lower.bounds[1L] !=
+       -Inf || upper.bounds[1L] != Inf) && method != "L-BFGS-B") {
+    warning("bounds can only be used with method L-BFGS-B")
+    method <- "L-BFGS-B"
+  }
+
+  theta <- c(start$mu, start$sigma)
+
+  # names for mean vector elements : mu_i
+  nmmu     <- "mu_1"
+  # names for sigma elements : sigma_ij
+  nmsigma  <- "sigma_1"
+  names(theta) <- c(nmmu, nmsigma)
+
+  negloglik <- function(x)
+  {
+    nf <- names(formals())
+
+    # recover parameter vector from named arguments (mu1=...,mu2=...,sigma11,sigma12 etc).
+    # stack all named arguments to parameter vector theta
+    theta <- sapply(nf, function(x) {eval(parse(text=x))})
+
+    # mean vector herholen
+    mean <- theta[1]
+
+    # Matrix für sigma bauen
+    sigma <- theta[-1]
+
+    # Log-Likelihood
+    # Wieso hier nur dmvnorm() : Wegen Dichte = Conditional density
+    f <- -(sum(weights*dnorm(X, mean, sigma, log=TRUE)) - length(X) *
+             log(pnorm(upper, mean=mean, sd=sigma) - pnorm(lower, mean=mean, sd=sigma)))
+
+    f
+  }
+  formals(negloglik) <- theta
+
+  # for method "L-BFGS-B" pass bounds parameter "lower.bounds" and "upper.bounds"
+  # under names "lower" and "upper"
+  if ((length(lower.bounds) > 1L || length(upper.bounds) > 1L || lower.bounds[1L] !=
+       -Inf || upper.bounds[1L] != Inf) && method == "L-BFGS-B") {
+    print(lower.bounds)
+    mle.fit <- eval.parent(substitute(mle(negloglik, start=as.list(theta), fixed=fixed,
+                                          method = method, lower=lower.bounds, upper=upper.bounds, ...)))
+
+
+    return (mle.fit)
+  }
+
+  return(NA)
+}
