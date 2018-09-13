be337696becef52b9f03313add1a9275a625835a
klin1234@gmail.com <klin1234@gmail.com>
2018-09-13 07:34:57 -0400

    Clean the package by deleting older dropout functions

--- singlecell/R/histogram.R
+++ singlecell/R/histogram.R
@@ -12,10 +12,10 @@
   break_vec <- break_vec + interval/2
   break_vec <- c(break_vec[1] - diff(break_vec)[1], break_vec)
 
-  zz <- hist(x, breaks = break_vec, col = "gray", ...)
+  zz <- graphics::hist(x, breaks = break_vec, col = "gray", ...)
   len <- length(which(x == min_val))
   if(len != 0){
-    rect(zz$breaks[1], 0, zz$breaks[2], len, col = rgb(0.803, 0.156, 0.211, 0.5))
+    graphics::rect(zz$breaks[1], 0, zz$breaks[2], len, col = grDevices::rgb(0.803, 0.156, 0.211, 0.5))
   }
 
   if(!all(is.na(param_list))){
@@ -32,14 +32,14 @@
     ymax <- max(unlist(density_list), na.rm = T)
 
     for(i in 1:length(density_list)){
-      lines(x_seq, density_list[[i]][[1]], col = rgb(0.803, 0.156, 0.211), lwd = lwd, lty = 1, ... )
-      lines(x_seq, density_list[[i]][[2]], col = rgb(0.584, 0.858, 0.564), lwd = lwd, lty = 1, ... )
+      graphics::lines(x_seq, density_list[[i]][[1]], col = grDevices::rgb(0.803, 0.156, 0.211), lwd = lwd, lty = 1, ... )
+      graphics::lines(x_seq, density_list[[i]][[2]], col = grDevices::rgb(0.584, 0.858, 0.564), lwd = lwd, lty = 1, ... )
     }
 
     i <- 1
     idx <- min(which(density_list[[i]][[1]] < density_list[[i]][[2]]))
     if(!is.na(idx)){
-      lines(rep(x_seq[idx], 2), c(1e6,-1e6), lwd = lwd, lty = 2, ...)
+      graphics::lines(rep(x_seq[idx], 2), c(1e6,-1e6), lwd = lwd, lty = 2, ...)
     }
   }
 
@@ -75,21 +75,21 @@
   col1 <- sapply(col1, function(x){1 - prob1 * (1 - x)})
   col2 <- sapply(col2, function(x){1 - prob2 * (1 - x)})
 
-  par(mar = c(1,1,3,0))
-  plot(NA, xlim = c(0,1), ylim = c(0,1), asp = T, axes = F,
+  graphics::par(mar = c(1,1,3,0))
+  graphics::plot(NA, xlim = c(0,1), ylim = c(0,1), asp = T, axes = F,
        xlab = "", ylab = "", ...)
 
-  title(xlab="Estimated label", line=0)
-  title(ylab="True label", line=0)
+  graphics::title(xlab="Estimated label", line=0)
+  graphics::title(ylab="True label", line=0)
 
-  rect(0, 0, 1, 1)
-  rect(0, .5, .5 , 1, col = rgb(col1[1], col1[2], col1[3]))
-  rect(.5, 0, 1 , .5, col = rgb(col2[1], col2[2], col2[3]))
+  graphics::rect(0, 0, 1, 1)
+  graphics::rect(0, .5, .5 , 1, col = grDevices::rgb(col1[1], col1[2], col1[3]))
+  graphics::rect(.5, 0, 1 , .5, col = grDevices::rgb(col2[1], col2[2], col2[3]))
 
-  text(0.25,0.75, labels = paste0("Dropout,\nn = ", tab[1,1], "\n(", round(prob1*100), "%)"))
-  text(0.75,0.75, labels = paste0("n = ", tab[1,2], "\n(", 100-round(prob1*100), "%)"))
-  text(0.75,0.25, labels = paste0("Not dropout, \nn = ", tab[2,2], "\n(", round(prob2*100), "%)"))
-  text(0.25,0.25, labels = paste0("n = ", tab[2,1], "\n(", 100-round(prob2*100), "%)"))
+  graphics::text(0.25,0.75, labels = paste0("Dropout,\nn = ", tab[1,1], "\n(", round(prob1*100), "%)"))
+  graphics::text(0.75,0.75, labels = paste0("n = ", tab[1,2], "\n(", 100-round(prob1*100), "%)"))
+  graphics::text(0.75,0.25, labels = paste0("Not dropout, \nn = ", tab[2,2], "\n(", round(prob2*100), "%)"))
+  graphics::text(0.25,0.25, labels = paste0("n = ", tab[2,1], "\n(", 100-round(prob2*100), "%)"))
 
   invisible()
 }
