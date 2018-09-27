d8106cb52f4e43bafea21606a22a929f8977a002
klin1234@gmail.com <klin1234@gmail.com>
2018-09-27 08:46:01 -0400

    Added a lot of tests

--- singlecell/R/matrix_factorization.R
+++ singlecell/R/matrix_factorization.R
@@ -3,13 +3,15 @@
   init <- .initialization(dat)
   u_mat <- init$u_mat; v_mat <- init$v_mat
 
-  current_obj <- .evaluate_objective(dat, u_mat, v_mat)
-  next_obj <- Inf
+  current_obj <- Inf
+  next_obj <- .evaluate_objective(dat, u_mat, v_mat)
   iter <- 1
 
   while(abs(current_obj - next_obj) > 1e-6 | iter < max_iter){
+    current_obj <- next_obj
+
     u_mat <- .optimize_mat(dat, u_mat, v_mat, left = T)
-    v_mat <- .optimize_mat(dat, v_mat, u_mat, left = T)
+    v_mat <- .optimize_mat(dat, v_mat, u_mat, left = F)
 
     next_obj <- .evaluate_objective(dat, u_mat, v_mat)
 
@@ -19,8 +21,7 @@
   return(u_mat = u_mat, v_mat = v_mat)
 }
 
-.evaluate_objective <- function(dat, u_mat, v_mat,
-                                tol = 1e-6){
+.evaluate_objective <- function(dat, u_mat, v_mat){
   stopifnot(nrow(dat) == nrow(u_mat), ncol(dat) == nrow(v_mat),
             ncol(u_mat) == ncol(v_mat))
   pred_mat <- u_mat %*% t(v_mat)
@@ -29,8 +30,7 @@
   sum(pred_mat[idx]*(1-dat[idx]))
 }
 
-.evaluate_objective_single <- function(dat_vec, current_vec, other_mat,
-                                       tol = 1e-6){
+.evaluate_objective_single <- function(dat_vec, current_vec, other_mat){
   stopifnot(length(current_vec) == ncol(other_mat))
   stopifnot(length(dat_vec) == nrow(other_mat))
 
@@ -52,8 +52,8 @@
   prod_mat <- u_mat %*% t(v_mat)
   svd_res <- svd(prod_mat)
 
-  u_mat <- svd_res$u %*% diag(sqrt(svd_res$d))
-  v_mat <- svd_res$v %*% diag(sqrt(svd_res$d))
+  u_mat <- svd_res$u[,1:k] %*% diag(sqrt(svd_res$d[1:k]))
+  v_mat <- svd_res$v[,1:k] %*% diag(sqrt(svd_res$d[1:k]))
 
   # project v back into positive space based on u
   for(j in 1:nrow(v_mat)){
@@ -66,7 +66,7 @@
 #########
 
 .optimize_mat <- function(dat, current_mat, other_mat, left = T){
-  stopifnot(length(current_vec) == ncol(other_mat))
+  stopifnot(ncol(current_mat) == ncol(other_mat))
   if(left) {
     stopifnot(ncol(dat) == nrow(other_mat))
   } else {
@@ -75,18 +75,22 @@
 
   for(i in 1:nrow(current_mat)){
     if(left) {dat_vec <- dat[i,]} else {dat_vec <- dat[,i]}
-    current_mat[i,] <- .optimize_row(dat_vec, current_mat[i,], other_mat)
+    if(any(!is.na(dat_vec))) current_mat[i,] <- .optimize_row(dat_vec, current_mat[i,], other_mat)
   }
 
   current_mat
 }
 
 .optimize_row <- function(dat_vec, current_vec, other_mat, max_iter = 100){
-  current_obj <- .evaluate_objective_single(dat_vec, current_vec, other_mat)
-  next_obj <- Inf
+  stopifnot(length(which(!is.na(dat_vec))) > 0)
+
+  current_obj <- Inf
+  next_obj <- .evaluate_objective_single(dat_vec, current_vec, other_mat)
   iter <- 1
 
   while(abs(current_obj - next_obj) > 1e-6 | iter < max_iter){
+    current_obj <- next_obj
+
     grad_vec <- .gradient_vec(dat_vec, current_vec, other_mat)
     stepsize <- .backtrack_linesearch(dat_vec, current_vec, other_mat, grad_vec)
 
@@ -117,8 +121,8 @@
   t_current <- max(t_current - tol, t_current/2)
 
   while(TRUE){
-    obj1 <- .evaluate_objective_single(current_vec - t_current*grad_vec)
-    obj2 <- .evaluate_objective_single(current_vec) - alpha*t_current*.l2norm(grad_vec)^2
+    obj1 <- .evaluate_objective_single(dat_vec, current_vec - t_current*grad_vec, other_mat)
+    obj2 <- .evaluate_objective_single(dat_vec, current_vec, other_mat) - alpha*t_current*.l2norm(grad_vec)^2
 
     if(obj1 > obj2) t_current <- t_current*beta else break()
   }
@@ -147,7 +151,7 @@
   }))
   const_mat <- rbind(const_mat, const_mat2)
 
-  const_dir <- c(rep(">=", 2*k), rep("<=", nrow(other_mat)))
+  const_dir <- c(rep(">=", 2*k), rep(">=", nrow(other_mat)))
   const_rhs <- c(current_vec, -current_vec, rep(0, nrow(other_mat)))
 
   res <- lpSolve::lp("min", objective_in, const_mat, const_dir, const_rhs)
--- singlecell/tests/testthat/test_matrix_factorization.R
+++ singlecell/tests/testthat/test_matrix_factorization.R
@@ -89,8 +89,8 @@ test_that(".evaluate_objective yields a smaller value under truth", {
 
     res <- .evaluate_objective(dat_mat, u_mat, v_mat)
 
-    u_mat <- abs(matrix(rnorm(20), nrow = 10, ncol = 2))
-    v_mat <- abs(matrix(rnorm(8), nrow = 4, ncol = 2))
+    u_mat2 <- abs(matrix(rnorm(20), nrow = 10, ncol = 2))
+    v_mat2 <- abs(matrix(rnorm(8), nrow = 4, ncol = 2))
     res2 <- .evaluate_objective(dat_mat, u_mat2, v_mat2)
 
     c(res, res2)
@@ -138,8 +138,8 @@ test_that(".evaluate_objective_single yields a smaller value under truth", {
     i <- sample(1:10, 1)
     res <- .evaluate_objective(dat_mat[i,], u_mat[i,], v_mat)
 
-    u_mat <- abs(matrix(rnorm(20), nrow = 10, ncol = 2))
-    v_mat <- abs(matrix(rnorm(8), nrow = 4, ncol = 2))
+    u_mat2 <- abs(matrix(rnorm(20), nrow = 10, ncol = 2))
+    v_mat2 <- abs(matrix(rnorm(8), nrow = 4, ncol = 2))
     res2 <- .evaluate_objective(dat_mat[i,], u_mat2[i,], v_mat2)
 
     c(res, res2)
@@ -149,5 +149,309 @@ test_that(".evaluate_objective_single yields a smaller value under truth", {
 })
 
 ################
+
+## .projection_l1 is correct
+
+test_that(".projection_l1 works", {
+  set.seed(10)
+  current_vec <- rnorm(10)
+  other_mat <- matrix(rnorm(50), ncol = 10)
+  res <- .projection_l1(current_vec, other_mat)
+
+  expect_true(is.numeric(res))
+  expect_true(!is.matrix(res))
+  expect_true(length(res) == length(current_vec))
+})
+
+test_that(".projection_l1 maintains greater than 0", {
+  set.seed(15)
+  current_vec <- rnorm(10)
+  other_mat <- matrix(rnorm(50), ncol = 10)
+  res <- .projection_l1(current_vec, other_mat)
+
+  expect_true(all(other_mat %*% res >= -1e-6))
+})
+
+test_that(".projection_l1 can keep the current vector", {
+  set.seed(20)
+  current_vec <- abs(rnorm(10))
+  other_mat <- abs(matrix(rnorm(50), ncol = 10))
+  res <- .projection_l1(current_vec, other_mat)
+
+  expect_true(sum(abs(res - current_vec)) < 1e-6)
+})
+
+test_that(".projection_l1 is actually a projection compared to the all 0 vector", {
+  trials <- 100
+
+  bool_vec <- sapply(1:trials, function(x){
+    set.seed(x)
+    current_vec <- abs(rnorm(10))
+    other_mat <- abs(matrix(rnorm(50), ncol = 10))
+    new_vec <- .projection_l1(current_vec, other_mat)
+
+    sum(abs(current_vec - new_vec)) <= sum(abs(current_vec))
+  })
+
+  expect_true(all(bool_vec))
+})
+
+#############
+
+## .initialization is correct
+
+test_that(".initialization works", {
+  set.seed(10)
+  dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+  dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+  res <- .initialization(dat)
+
+  expect_true(is.list(res))
+  expect_true(ncol(res$u_mat) == ncol(res$v_mat))
+  expect_true(nrow(res$u_mat) == nrow(dat))
+  expect_true(nrow(res$v_mat) == ncol(dat))
+})
+
+test_that(".initialization actually gives positive predictions", {
+  set.seed(20)
+  dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+  dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+  res <- .initialization(dat)
+  pred_mat <- res$u_mat %*% t(res$v_mat)
+
+  expect_true(all(pred_mat[which(!is.na(dat))] > -1e6))
+})
+
+#########################
+
+## .backtrack_linesearch is correct
+
+test_that(".backtrack_linesearch works", {
+  set.seed(20)
+  dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+  dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+  res <- .initialization(dat)
+  u_mat <- res$u_mat
+  v_mat <- res$v_mat
+  i <- 1
+  grad_vec <- .gradient_vec(dat[i,], u_mat[i,], v_mat)
+
+  res <- .backtrack_linesearch(dat[i,], u_mat[i,], v_mat, grad_vec)
+
+  expect_true(is.numeric(res))
+  expect_true(!is.matrix(res))
+  expect_true(length(res) == 1)
+})
+
+test_that(".backtrack_linesearch actually keeps the positive constraint", {
+  trials <- 50
+
+  bool_vec <- sapply(1:trials, function(x){
+    set.seed(10*x)
+
+    dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+    dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+    res <- .initialization(dat)
+    u_mat <- res$u_mat
+    v_mat <- res$v_mat
+    i <- sample(1:10, 1)
+
+    if(any(!is.na(dat[i,]))){
+      grad_vec <- .gradient_vec(dat[i,], u_mat[i,], v_mat)
+
+      res <- .backtrack_linesearch(dat[i,], u_mat[i,], v_mat, grad_vec)
+
+      u_new <- u_mat[i,] - res*grad_vec
+      all(v_mat %*% u_new >= -1e-6)
+    } else {
+      TRUE
+    }
+  })
+
+  expect_true(all(bool_vec))
+})
+
+
+test_that(".backtrack_linesearch lowers the objective", {
+  trials <- 50
+
+  bool_vec <- sapply(1:trials, function(x){
+    set.seed(10*x)
+
+    dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+    dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+    res <- .initialization(dat)
+    u_mat <- res$u_mat
+    v_mat <- res$v_mat
+
+    i <- sample(1:10, 1)
+    if(any(!is.na(dat[i,]))){
+      obj1 <- .evaluate_objective_single(dat[i,], u_mat[i,], v_mat)
+      grad_vec <- .gradient_vec(dat[i,], u_mat[i,], v_mat)
+
+      res <- .backtrack_linesearch(dat[i,], u_mat[i,], v_mat, grad_vec)
+
+      u_new <- u_mat[i,] - res*grad_vec
+      obj2 <- .evaluate_objective_single(dat[i,], u_new, v_mat)
+
+      obj2 <= obj1 + 1e-6
+    } else {
+      TRUE
+    }
+  })
+
+  expect_true(all(bool_vec))
+})
+
+##################
+
+## .optimize_row is correct
+
+test_that(".optimize_row works", {
+  set.seed(20)
+  dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+  dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+  res <- .initialization(dat)
+  u_mat <- res$u_mat
+  v_mat <- res$v_mat
+  i <- 1
+
+  res <- .optimize_row(dat[i,], u_mat[i,], v_mat)
+
+  expect_true(is.numeric(res))
+  expect_true(length(res) == length(u_mat[i,]))
+})
+
+test_that(".optimize_row actually lowers the objective", {
+  trials <- 25
+
+  bool_vec <- sapply(1:trials, function(x){
+    set.seed(x*10)
+    dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+    dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+    res <- .initialization(dat)
+    u_mat <- res$u_mat
+    v_mat <- res$v_mat
+    i <- sample(1:10, 1)
+
+    if(any(!is.na(dat[i,]))){
+      u_new <- .optimize_row(dat[i,], u_mat[i,], v_mat)
+      obj1 <- .evaluate_objective_single(dat[i,], u_mat[i,], v_mat)
+      obj2 <- .evaluate_objective_single(dat[i,], u_new, v_mat)
+
+      obj2 <= obj1 + 1e-6
+    } else {TRUE}
+  })
+
+  expect_true(all(bool_vec))
+})
+
+test_that(".optimize_row works the other way", {
+  trials <- 25
+
+  bool_vec <- sapply(1:trials, function(x){
+    set.seed(x*10)
+    dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+    dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+    res <- .initialization(dat)
+    u_mat <- res$u_mat
+    v_mat <- res$v_mat
+    j <- sample(1:4, 1)
+
+    if(any(!is.na(dat[,j]))){
+      v_new <- .optimize_row(dat[,j], v_mat[j,], u_mat)
+      obj1 <- .evaluate_objective_single(dat[,j], v_mat[j,], u_mat)
+      obj2 <- .evaluate_objective_single(dat[,j], v_new, u_mat)
+
+      obj2 <= obj1 + 1e-6
+    } else {TRUE}
+  })
+
+  expect_true(all(bool_vec))
+})
+
+##################
+
+## .optimize_mat is correct
+
+test_that(".optimize_mat works", {
+  set.seed(20)
+  dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+  dat[sample(1:prod(dim(dat)), 10)] <- NA
+
+  res <- .initialization(dat)
+  u_mat <- res$u_mat
+  v_mat <- res$v_mat
+
+  res <- .optimize_mat(dat, u_mat, v_mat)
+
+  expect_true(is.matrix(res))
+  expect_true(all(dim(res) == dim(u_mat)))
+})
+
+test_that(".optimize_mat keeps the positive constraint", {
+  trials <- 10
+
+  bool_vec <- sapply(1:trials, function(x){
+    set.seed(x*10)
+    dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+    dat[sample(1:prod(dim(dat)), 10)] <- NA
+    bool <- sample(c(T, F), 1)
+
+    res <- .initialization(dat)
+    u_mat <- res$u_mat
+    v_mat <- res$v_mat
+    if(bool){
+      res <- .optimize_mat(dat, u_mat, v_mat, bool)
+      pred_mat <- res %*% t(v_mat)
+    } else {
+      res <- .optimize_mat(dat, v_mat, u_mat, bool)
+      pred_mat <- u_mat %*% t(res)
+    }
+
+    idx <- which(!is.na(dat))
+
+    all(pred_mat[idx] >= -1e-6)
+  })
+
+  expect_true(all(bool_vec))
+})
+
+
+test_that(".optimize_mat lowers the objective value", {
+  trials <- 10
+
+  bool_vec <- sapply(1:trials, function(x){
+    set.seed(x*10)
+    dat <- abs(matrix(rnorm(40), nrow = 10, ncol = 4))
+    dat[sample(1:prod(dim(dat)), 10)] <- NA
+    bool <- sample(c(T, F), 1)
+
+    res <- .initialization(dat)
+    u_mat <- res$u_mat
+    v_mat <- res$v_mat
+    obj1 <- .evaluate_objective(dat, u_mat, v_mat)
+
+    if(bool){
+      res <- .optimize_mat(dat, u_mat, v_mat, bool)
+      obj2 <- .evaluate_objective(dat, res, v_mat)
+    } else {
+      res <- .optimize_mat(dat, v_mat, u_mat, bool)
+      obj2 <- .evaluate_objective(dat, u_mat, res)
+    }
+
+    obj2 <= obj1 + 1e-6
+  })
+
+  expect_true(all(bool_vec))
+})
 
 
