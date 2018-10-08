fb466d6922fe352d6cbdba810dbcc095d733a551
klin1234@gmail.com <klin1234@gmail.com>
2018-10-08 08:57:07 -0400

    Starting week 24

--- experiment/Week23_simulation.R
+++ experiment/Week23_simulation.R
@@ -1,6 +1,6 @@
 rm(list=ls())
-load("../experiment/Week23_simulation.RData")
 source("../experiment/Week23_simulation_generator.R")
+load("../experiment/Week23_simulation.RData")
 
 col_vec <- c(rgb(205,40,54,maxColorValue=255), #red
              rgb(180,200,255,maxColorValue=255), #purple
@@ -75,35 +75,118 @@ graphics.off()
 # plot(sort(zz))
 # plot(sort(dat[dat != 0]))
 
-
 ###########
 
 # try the naive thing
-dat_inv <- dat
-dat_inv[which(dat > 0)] <- 1/dat[which(dat > 0)]
-dat_inv[which(dat == 0)] <- max(dat_inv)
-
-svd_res <- svd(dat_inv)
-k <- 2
+svd_res <- svd(dat)
+k <- 5
 u_mat_naive <- svd_res$u[,1:k] %*% diag(sqrt(svd_res$d[1:k]))
 v_mat_naive <- svd_res$v[,1:k] %*% diag(sqrt(svd_res$d[1:k]))
-library(slingshot)
-slingshot_res <- slingshot::slingshot(data = u_mat_naive, clusterLabels = rep(1:simulation$h, each = simulation$n_each))
+pred_mat <- u_mat_naive %*% t(v_mat_naive)
+svd_res <- svd(-1/pred_mat)
+u_mat_naive2 <- svd_res$u[,1:k] %*% diag(sqrt(svd_res$d[1:k]))
 
-plot(u_mat_naive[,1], u_mat_naive[,2],
+png("../figure/experiment/23_naive.png", height = 1200, width = 2000, res = 300, units = "px")
+par(mfrow = c(1,2))
+plot(u_mat_naive[,1],u_mat_naive[,2],
      col = col_vec[rep(1:simulation$h, each = simulation$n_each)], asp = T,
-     pch = 16, xlab = "Estimated dim. 1", ylab = "Estimated dim. 2", main = "Cell estimated vectors")
-lines(slingshot_res)
+     xlim = range(c(u_mat_naive[,1], 0)),
+     ylim = range(c(u_mat_naive[,2], 0)),
+     pch = 16, xlab = "Estimated dim. 1", ylab = "Estimated dim. 2",
+     main = "Naive cell estimated vectors\n(Mean)")
+lines(c(-1e6, 1e6), rep(0, 2), col = "red", lwd = 2, lty = 2)
+lines( rep(0, 2), c(-1e6, 1e6), col = "red", lwd = 2, lty = 2)
 
-# let's see how well this naive method did
-.evaluate_objective(dat, u_mat_naive, v_mat_naive)
+plot(init$u_mat[,1], init$u_mat[,2],
+     col = col_vec[rep(1:simulation$h, each = simulation$n_each)], asp = T,
+     xlim = range(c(init$u_mat[,1], 0)),
+     ylim = range(c(init$u_mat[,2], 0)),
+     pch = 16, xlab = "Estimated dim. 1", ylab = "Estimated dim. 2",
+     main = "Naive cell estimated vectors\n(Negative reciprocal of mean)")
+lines(c(-1e6, 1e6), rep(0, 2), col = "red", lwd = 2, lty = 2)
+lines( rep(0, 2), c(-1e6, 1e6), col = "red", lwd = 2, lty = 2)
+graphics.off()
+
+png("../figure/experiment/23_naive2.png", height = 1200, width = 1000, res = 300, units = "px")
+plot(u_mat_naive2[,1],u_mat_naive2[,2],
+     col = col_vec[rep(1:simulation$h, each = simulation$n_each)], asp = T,
+     xlim = range(c(u_mat_naive2[,1], 0)),
+     ylim = range(c(u_mat_naive2[,2], 0)),
+     pch = 16, xlab = "Estimated dim. 1", ylab = "Estimated dim. 2",
+     main = "Naive cell estimated vectors\n(Mean)")
+lines(c(-1e6, 1e6), rep(0, 2), col = "red", lwd = 2, lty = 2)
+lines( rep(0, 2), c(-1e6, 1e6), col = "red", lwd = 2, lty = 2)
+graphics.off()
 
 ##############
 
-# compare to our implemented method
-.evaluate_objective(dat, res_nodropout$u_mat, res_nodropout$v_mat)
+png("../figure/experiment/23_nodropout.png", height = 1200, width = 2000, res = 300, units = "px")
+par(mfrow = c(1,2))
+slingshot_res <- slingshot::slingshot(data = res_nodropout$u_mat,
+                                      clusterLabels = rep(1:simulation$h, each = simulation$n_each),
+                                      omega = 6)
 
 plot(res_nodropout$u_mat[,1], res_nodropout$u_mat[,2],
      col = col_vec[rep(1:simulation$h, each = simulation$n_each)], asp = T,
-     pch = 16, xlab = "Estimated dim. 1", ylab = "Estimated dim. 2", main = "Cell estimated vectors")
+     xlim = range(c(res_nodropout$u_mat[,1], 0)),
+     ylim = range(c(res_nodropout$u_mat[,2], 0)),
+     pch = 16, xlab = "Estimated dim. 1", ylab = "Estimated dim. 2",
+     main = "Our cell estimated vectors\n(Vanilla)")
+lines(c(-1e6, 1e6), rep(0, 2), col = "red", lwd = 2, lty = 2)
+lines( rep(0, 2), c(-1e6, 1e6), col = "red", lwd = 2, lty = 2)
 
+lines(slingshot_res)
+
+pred_mat <- res_nodropout$u_mat %*% t(res_nodropout$v_mat)
+plot(as.numeric(simulation$gram_mat), as.numeric(pred_mat), asp = T,
+     col = rgb(0,0,0,0.1), pch = 16, xlab = "Latent inner product",
+     ylab = "Predicted inner product", main = "Our estimated inner product\n(Vanilla)")
+lines(c(-1e6, 1e6), c(-1e6, 1e6), col = "red", lwd = 2, lty =2)
+
+graphics.off()
+
+##############
+
+png("../figure/experiment/23_withdropout.png", height = 1200, width = 1000, res = 300, units = "px")
+plot(res_withdropout$u_mat[,1], res_withdropout$u_mat[,2],
+     col = col_vec[rep(1:simulation$h, each = simulation$n_each)], asp = T,
+     xlim = range(c(res_withdropout$u_mat[,1], 0)),
+     ylim = range(c(res_withdropout$u_mat[,2], 0)),
+     pch = 16, xlab = "Estimated dim. 1", ylab = "Estimated dim. 2",
+     main = "Our cell estimated vectors\n(Accounting dropout)")
+lines(c(-1e6, 1e6), rep(0, 2), col = "red", lwd = 2, lty = 2)
+lines( rep(0, 2), c(-1e6, 1e6), col = "red", lwd = 2, lty = 2)
+graphics.off()
+
+
+png("../figure/experiment/23_withdropout_pred.png", height = 1200, width = 2000, res = 300, units = "px")
+par(mfrow = c(1,2))
+pred_mat <- res_withdropout$u_mat %*% t(res_withdropout$v_mat)
+idx <- which(is.na(zero_mat))
+plot(as.numeric(simulation$gram_mat)[-idx], as.numeric(pred_mat)[-idx], asp = T,
+     col = rgb(0,0,0,0.1), pch = 16, xlab = "Latent inner product",
+     ylab = "Predicted inner product", main = "Our estimated inner product\n(Account dropout)")
+lines(c(-1e6, 1e6), c(-1e6, 1e6), col = "red", lwd = 2, lty =2)
+
+plot(as.numeric(simulation$gram_mat)[idx], as.numeric(pred_mat)[idx], asp = T,
+     col = rgb(0,0,1,0.1), pch = 16, xlab = "Latent inner product",
+     ylab = "Predicted inner product", main = "Our estimated inner product\n(Account dropout)")
+lines(c(-1e6, 1e6), c(-1e6, 1e6), col = "red", lwd = 2, lty =2)
+graphics.off()
+
+png("../figure/experiment/23_withdropout_imputed.png", height = 1200, width = 2000, res = 300, units = "px")
+idx <- which(is.na(zero_mat))
+ylim <- range(c(as.numeric(simulation$dat)[idx]), as.numeric(dat_impute)[idx])
+par(mfrow = c(1,2))
+plot(as.numeric(-1/simulation$gram_mat)[idx], as.numeric(simulation$dat)[idx], asp = T,
+     ylim = ylim,
+     col = rgb(0,0,0,0.1), pch = 16, xlab = "Expected value",
+     ylab = "Observed value", main = "Before imputation")
+lines(c(-1e6, 1e6), c(-1e6, 1e6), col = "red", lwd = 2, lty =2)
+
+plot(as.numeric(-1/simulation$gram_mat)[idx], as.numeric(dat_impute)[idx], asp = T,
+     ylim = ylim,
+     col = rgb(0,0,0,0.1), pch = 16, xlab = "Expected value",
+     ylab = "Imputed value", main = "After imputation")
+lines(c(-1e6, 1e6), c(-1e6, 1e6), col = "red", lwd = 2, lty =2)
+graphics.off()
