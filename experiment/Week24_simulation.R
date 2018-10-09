fb6c9f28089ba7ddb9734200b6520c9d57e83cc0
klin1234@gmail.com <klin1234@gmail.com>
2018-10-09 07:20:15 -0400

    Trying the exponential version

--- experiment/Week24_simulation.R
+++ experiment/Week24_simulation.R
@@ -24,16 +24,41 @@ lines( rep(0, 2), c(-1e6, 1e6), col = "red", lwd = 2, lty = 2)
 
 ##################
 
+par(mfrow = c(1,2))
 plot(res_nodropout$u_mat[,1], res_nodropout$u_mat[,2],
      xlim = range(c(res_nodropout$u_mat[,1], 0)),
      ylim = range(c(res_nodropout$u_mat[,2], 0)),
      col = col_vec[rep(1:simulation$h, each = simulation$n_each)], asp = T,
      pch = 16, xlab = "Latent dim. 1", ylab = "Latent dim. 2", main = "Cell latent vectors")
+pred_mat <- res_nodropout$u_mat %*% t(res_nodropout$v_mat)
+plot(as.numeric(simulation$gram_mat), pred_mat, asp = T)
+lines(c(-1e5, 1e5), c(-1e5, 1e5), col = "red", lwd = 2, lty = 2)
 
-##################
+par(mfrow = c(1,2))
+plot(res_withdropout$u_mat[,1], res_withdropout$u_mat[,2],
+     xlim = range(c(res_withdropout$u_mat[,1], 0)),
+     ylim = range(c(res_withdropout$u_mat[,2], 0)),
+     col = col_vec[rep(1:simulation$h, each = simulation$n_each)], asp = T,
+     pch = 16, xlab = "Latent dim. 1", ylab = "Latent dim. 2", main = "Cell latent vectors")
+pred_mat <- res_withdropout$u_mat %*% t(res_withdropout$v_mat)
+plot(as.numeric(simulation$gram_mat), pred_mat, asp = T)
+lines(c(-1e5, 1e5), c(-1e5, 1e5), col = "red", lwd = 2, lty = 2)
+
+par(mfrow = c(1,2))
+plot(res_withimpute$u_mat[,1], res_withimpute$u_mat[,2],
+     xlim = range(c(res_withimpute$u_mat[,1], 0)),
+     ylim = range(c(res_withimpute$u_mat[,2], 0)),
+     col = col_vec[rep(1:simulation$h, each = simulation$n_each)], asp = T,
+     pch = 16, xlab = "Latent dim. 1", ylab = "Latent dim. 2", main = "Cell latent vectors")
+pred_mat <- res_withimpute$u_mat %*% t(res_withimpute$v_mat)
+plot(as.numeric(simulation$gram_mat), pred_mat, asp = T)
+lines(c(-1e5, 1e5), c(-1e5, 1e5), col = "red", lwd = 2, lty = 2)
+
+####################
 
 zero_mat2 <- zero_mat
 zero_mat2[is.na(zero_mat2)] <- 2
+length(which(zero_mat2 == 0))/prod(dim(zero_mat2))
 
 par(mar = rep(0.5,4))
 image(.rotate(zero_mat2), breaks = c(-0.5,0.5,1.5,2.5), col = c("blue3", "gold", "firebrick1"), asp = nrow(zero_mat)/ncol(zero_mat),
--- experiment/Week24_simulation_script.R
+++ experiment/Week24_simulation_script.R
@@ -3,35 +3,35 @@ source("../experiment/Week24_simulation_generator.R")
 library(singlecell)
 
 set.seed(10)
-simulation <- .data_generator(total = 200, col_drop = F)
+simulation <- .data_generator_exponential(total = 200, col_drop = F)
 dat <- simulation$dat
 
-init <- singlecell:::.initialization(dat, family = "gaussian")
+init <- singlecell:::.initialization(dat, family = "exponential")
 res_nodropout <- singlecell:::.fit_factorization(dat, init$u_mat, init$v_mat,
-                                                 verbose = T, family = "gaussian",
+                                                 verbose = T, family = "exponential",
                                                  cores = 15)
 
-save.image("Week24_simulation.RData")
+save.image("Week24_simulation_exponential.RData")
 
 dropout_mat <- singlecell:::.dropout(dat)
-zero_mat <- singlecell:::.find_true_zeros(dropout_mat, num_neighbors = 5)
+zero_mat <- singlecell:::.find_true_zeros(dropout_mat, num_neighbors = 15)
 
 dat2 <- dat
 dat2[which(is.na(zero_mat))] <- NA
 dat_impute <- singlecell:::.scImpute(dat, which(is.na(zero_mat)), Kcluster = 4,
                                      max_time = 5, verbose = T)
 
-save.image("Week24_simulation.RData")
+save.image("Week24_simulation_exponential.RData")
 
-init_impute <- singlecell:::.initialization(dat_impute, family = "gaussian")
+init_impute <- singlecell:::.initialization(dat_impute, family = "exponential")
 res_withdropout <- singlecell:::.fit_factorization(dat2, init_impute$u_mat, init_impute$v_mat,
-                                                   verbose = T, family = "gaussian",
+                                                   verbose = T, family = "exponential",
                                                    cores = 15)
 
-save.image("Week24_simulation.RData")
+save.image("Week24_simulation_exponential.RData")
 
 res_withimpute <- singlecell:::.fit_factorization(dat_impute, init_impute$u_mat, init_impute$v_mat,
-                                                  verbose = T, family = "gaussian",
+                                                  verbose = T, family = "exponential",
                                                   cores = 15)
 
-save.image("Week24_simulation.RData")
+save.image("Week24_simulation_exponential.RData")
