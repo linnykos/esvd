98b207c42f3487e271bdc5111d79001ae7425396
klin1234@gmail.com <klin1234@gmail.com>
2018-11-13 13:22:56 -0500

    Discovered some bugs with slingshot...

--- simulation/wasserstein_example.R
+++ simulation/wasserstein_example.R
@@ -37,7 +37,7 @@ library(singlecell)
   cell_mat <- svd_res$u[,1:k] %*% diag(sqrt(svd_res$d[1:k]))
   gene_mat <- svd_res$v[,1:k] %*% diag(sqrt(svd_res$d[1:k]))
 
-  res <- .identification(cell_mat, gene_mat)
+  res <- .reparameterize(cell_mat, gene_mat)
   cell_mat <- res$X; gene_mat <- res$Y
 
   obs_mat <- matrix(0, ncol = ncol(gram_mat), nrow = nrow(gram_mat))
@@ -61,45 +61,37 @@ col_vec <- c(rgb(205,40,54,maxColorValue=255), #red
 
 #############################
 
+n_seq <- round(seq(10, 50, length.out = 4))
+d_seq <- round(seq(20, 100, length.out = 4))
+res_list <- vector("list", 4)
+
+for(i in 1:4){
+  set.seed(10)
+  res <- .data_generator(n_each = n_seq[i], d_each = d_seq[i], multiplier = 0.1)
+  dat <- res$dat
+
+  # real analysis
+  init <- .initialization(dat, family = "gaussian", max_val = 10)
+  res_list[[i]] <- .fit_factorization(dat, init$u_mat, init$v_mat,
+                            max_val = 5, family = "gaussian", verbose = T,
+                            max_iter = 10, reparameterize = T,
+                            return_path = F)
+}
+
+png("../figure/simulation/example_trajectories.png", height = 800, width = 3000, res = 300, units = "px")
+par(mfrow = c(1,4), mar = c(4,4,4,1))
 set.seed(10)
-res <- .data_generator(n_each = 50, d_each = 120, multiplier = 0.1)
-stopifnot(sum(abs(t(res$cell_mat)%*%res$cell_mat - t(res$gene_mat)%*%res$gene_mat)) <= 1e-6)
-stopifnot(sum(abs((t(res$cell_mat)%*%res$cell_mat)[2:3])) <= 1e-6)
-dat <- res$dat
-# quantile(dat)
-# .plot_singlecell(res$dat)
-plot(res$cell_mat[,1], res$cell_mat[,2], pch = 16, asp = T,
-     col = col_vec[rep(1:4, each = res$n_each)])
+n_pop <- 500
+res <- .data_generator(n_each = n_pop, d_each = 50, multiplier = 0.1)
+tmp <- res$cell_mat; svd_res <- svd(tmp); tmp <- svd_res$u[,1:res$k] %*% diag(svd_res$d[1:res$k])
+plot(tmp[,1], tmp[,2], pch = 16, col = col_vec[rep(1:4, each = n_pop)], asp = T)
 
-# naive analysis
-# svd_res <- svd(dat)
-# u_mat <- svd_res$u[,1:res$k] %*% diag(sqrt(svd_res$d[1:res$k]))
-# plot(u_mat[,1], u_mat[,2], pch = 16, asp = T,
-#      col = col_vec[rep(1:4, each = res$n_each)])
-
-# naive analysis of gram matrix
-# svd_res <- svd(res$gram_mat)
-# u_mat <- svd_res$u[,1:res$k] %*% diag(sqrt(svd_res$d[1:res$k]))
-# plot(u_mat[,1], u_mat[,2], pch = 16, asp = T,
-#      col = col_vec[rep(1:4, each = res$n_each)])
-
-# real analysis
-init <- .initialization(dat, family = "gaussian", max_val = 10)
-# plot(init$u_mat[,1], init$u_mat[,2], pch = 16, asp = T,
-#     col = col_vec[rep(1:4, each = res$n_each)])
-fit <- .fit_factorization(dat, init$u_mat, init$v_mat,
-                          max_val = 5, family = "gaussian", verbose = T,
-                          max_iter = 10, reparameterize = T,
-                          return_path = T)
-plot(fit$u_mat[,1], fit$u_mat[,2], pch = 16, asp = T,
-    col = col_vec[rep(1:4, each = res$n_each)])
-
-# ideal analysis
-# fit_2 <- .fit_factorization(dat, res$cell_mat, res$gene_mat,
-#                           max_val = 5, family = "gaussian", verbose = T,
-#                           max_iter = 50, reparameterize = T)
-# plot(fit_2$u_mat[,1], fit_2$u_mat[,2], pch = 16, asp = T,
-#      col = col_vec[rep(1:4, each = res$n_each)])
-# stopifnot(sum(abs(t(fit_2$u_mat)%*%fit_2$u_mat - t(fit_2$v_mat)%*%fit_2$v_mat)) <= 1e-6)
-# stopifnot(sum(abs((t(fit_2$u_mat)%*%fit_2$u_mat)[2:3])) <= 1e-6)
-#
+for(i in c(1,2,4)){
+  tmp <- res_list[[i]]$u_mat; svd_res <- svd(tmp); tmp <- svd_res$u[,1:res$k] %*% diag(svd_res$d[1:res$k])
+  plot(tmp[,1], tmp[,2],
+       pch = 16, col = col_vec[rep(1:4, each = n_seq[i])], asp = T,
+       xlab = "X[,1]", ylab = "X[,2]")
+  curves <- slingshot(tmp, cluster_labels = rep(1:4, each = n_seq[i]),
+                      starting_cluster = 1)
+}
+graphics.off()
