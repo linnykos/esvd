b0873b2ad3b6d069339a9c263f0ab57e5ec4c8e7
klin1234@gmail.com <klin1234@gmail.com>
2018-11-16 12:27:05 -0500

    Major revamp of the identifiability function
    I need to add the sample version, fix .reparmaterization and
      redo the tests

--- singlecell/R/identifiability.R
+++ singlecell/R/identifiability.R
@@ -1,27 +1,40 @@
-.identification <- function(X, Y){
-  stopifnot(ncol(X) == ncol(Y))
-
-  cov_x <- t(X) %*% X
-  cov_y <- t(Y) %*% Y
-
+.identification <- function(cov_x, cov_y, check = F){
   eigen_x <- eigen(cov_x)
   eigen_y <- eigen(cov_y)
 
-  Ux <- eigen_x$vectors
-  Uy <- eigen_y$vectors
-  Dx <- diag(eigen_x$values)
+  Vx <- eigen_x$vectors
+  Vy <- eigen_y$vectors
+  Dx <- diag(eigen_x$values); Dx_inv <- Dx; diag(Dx_inv) <- 1/diag(Dx)
   Dy <- diag(eigen_y$values)
 
-  inner_left <- sqrt(Dx) %*% t(Ux) %*% Uy %*% sqrt(Dy)
-  svd_res <- svd(inner_left)
-  Q <- svd_res$v %*% t(svd_res$u)
+  tmp <- t(Vx)%*%Vy%*%sqrt(Dy)
+  svd_tmp <- svd(tmp)
+  Theta <- svd_tmp$u %*% diag(svd_tmp$d) %*% t(svd_tmp$u) %*% sqrt(Dx_inv)
 
-  sym_prod <- Ux %*% t(Ux) %*% Uy %*% sqrt(Dy) %*% Q %*% diag(1/sqrt(diag(Dx))) %*% t(Ux)
+  # run a check
+  if(check){
+    Q <- svd_tmp$u %*% t(svd_tmp$v)
+    Theta2 <- t(Vx)%*%Vy%*%sqrt(Dy) %*% t(Q) %*% sqrt(Dx_inv)
+    stopifnot(sum(abs(Theta2 - Theta)) <= 1e-6)
+  }
+
+  sym_prod <- Vx %*% Theta %*% t(Vx)
+
+  if(check){
+    stopifnot(sum(abs(sym_prod - t(sym_prod))) <= 1e-6)
+  }
 
   eigen_sym <- eigen(sym_prod)
-  A <- diag(sqrt(eigen_sym$values)) %*% t(eigen_sym$vectors)
+  T_mat <- diag(sqrt(eigen_sym$values)) %*% t(eigen_sym$vectors)
 
-  X <- X %*% t(A); Y <- Y %*% solve(A)
+  if(check){
+    mat1 <- t(T_mat)%*%T_mat%*%Vx%*%sqrt(Dx)%*%Q
+    mat2 <- Vy %*% sqrt(Dy)
+    stopifnot(sum(abs(mat1 - mat2)) <= 1e-6)
+  }
 
-  list(X = X, Y = Y, A = A)
+  T_mat
 }
+
+# cov_x = matrix(c(2,1,1,2),2,2); cov_y = matrix(c(5,-1,-1,5),2,2)
+# T_mat %*% cov_x %*% t(T_mat); solve(t(T_mat)) %*% cov_y %*% solve(T_mat)
