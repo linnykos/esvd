de6eab7445acc933797c54e478e6b0a70416d4cb
klin1234@gmail.com <klin1234@gmail.com>
2018-11-22 11:51:32 -0500

    Adding the Wasserstein factorization

---
+++ main/step3_factorization.R
@@ -0,0 +1,9 @@
+load("../results/step1_imputing.RData")
+
+init <- singlecell:::.initialization(dat_impute, family = "gaussian", max_val = 100)
+res <- singlecell:::.fit_factorization(dat_impute, init$u_mat, init$v_mat,
+                                       max_val = 100, family = "gaussian", verbose = T,
+                                       max_iter = 50, reparameterize = T,
+                                       return_path = F)
+
+save.image("../results/step3_factorization.RData")
