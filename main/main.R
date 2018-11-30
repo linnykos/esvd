a73453f50f1c2571d9bb49cbebf1a78ce4445ab3
klin1234@gmail.com <klin1234@gmail.com>
2018-11-30 17:41:45 -0500

    Doing some more experimentation

--- main/main.R
+++ main/main.R
@@ -1,4 +1,3 @@
 source("../main/step0_screening.R")
 source("../main/step1_imputing.R")
 source("../main/step3_factorization.R")
-source("../main/step4_clustering.R")
--- main/step3_factorization.R
+++ main/step3_factorization.R
@@ -1,11 +1,12 @@
 load("../results/step1_imputing.RData")
 
-dat_impute_log <- log(dat_impute + 1)
+#dat_impute_log <- log(dat_impute + 1)
 
-init <- singlecell:::.initialization(dat_impute_log, family = "gaussian", max_val = 10,
+max_val <- 15
+init <- singlecell:::.initialization(dat_impute, family = "gaussian", max_val = max_val,
                                      k = 5)
-res <- singlecell:::.fit_factorization(dat_impute_log, init$u_mat, init$v_mat,
-                                       max_val = 10, family = "gaussian", verbose = T,
+res <- singlecell:::.fit_factorization(dat_impute, init$u_mat, init$v_mat,
+                                       max_val = max_val, family = "gaussian", verbose = T,
                                        max_iter = 25, reparameterize = T,
                                        return_path = F, cores = 10)
 
