5510f6f3afeffa1e83ff6968f87b704c6e8b5904
klin1234@gmail.com <klin1234@gmail.com>
2019-02-15 17:39:42 -0500

    Fixed the identifiability problem

--- singlecell/R/identifiability.R
+++ singlecell/R/identifiability.R
@@ -1,24 +1,24 @@
-.identification <- function(cov_x, cov_y, check = T){
+.identification <- function(cov_x, cov_y, check = F){
   eigen_x <- eigen(cov_x)
   eigen_y <- eigen(cov_y)
 
   Vx <- eigen_x$vectors
   Vy <- eigen_y$vectors
-  Dx <- diag(eigen_x$values); Dx_inv <- Dx; diag(Dx_inv) <- 1/diag(Dx)
+  Dx <- diag(eigen_x$values);
   Dy <- diag(eigen_y$values)
 
-  tmp <- t(Vx)%*%Vy%*%sqrt(Dy)
+  tmp <- sqrt(Dy) %*% t(Vy) %*% Vx %*% sqrt(Dx)
   svd_tmp <- svd(tmp)
-  R <- svd_tmp$u %*% diag(svd_tmp$d) %*% t(svd_tmp$u) %*% sqrt(Dx_inv)
+  R <- svd_tmp$u %*% t(svd_tmp$v)
 
   # run a check
   if(check){
-    Q <- svd_tmp$u %*% t(svd_tmp$v)
-    R2 <- t(Vx)%*%Vy%*%sqrt(Dy) %*% t(Q) %*% sqrt(Dx_inv)
-    stopifnot(sum(abs(R2 - R)) <= 1e-6)
+    Q <- t(R) %*% tmp
+    stopifnot(sum(abs(Q - t(Q))) <= 1e-6)
   }
 
-  sym_prod <- Vx %*% R %*% t(Vx)
+  Dx_inv <- Dx; diag(Dx_inv) <- 1/diag(Dx)
+  sym_prod <- Vx %*% sqrt(Dx_inv) %*% t(R) %*% sqrt(Dy) %*% t(Vy)
 
   if(check){
     stopifnot(sum(abs(sym_prod - t(sym_prod))) <= 1e-6)
@@ -28,8 +28,10 @@
   T_mat <- diag(sqrt(eigen_sym$values)) %*% t(eigen_sym$vectors)
 
   if(check){
-    mat1 <- t(T_mat)%*%T_mat%*%Vx%*%sqrt(Dx)%*%Q
-    mat2 <- Vy %*% sqrt(Dy)
+    mat1 <- T_mat %*% cov_x %*% t(T_mat)
+
+    T_mat_inv <- solve(T_mat)
+    mat2 <- t(T_mat_inv) %*% cov_y %*% T_mat_inv
     stopifnot(sum(abs(mat1 - mat2)) <= 1e-6)
   }
 
