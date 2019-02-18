44c0e8fd2cef492d520d2265af9c68b739b82f60
klin1234@gmail.com <klin1234@gmail.com>
2019-02-18 13:34:53 -0500

    Cleaning up the push

--- singlecell/tests/testthat/test_identifiability.R
+++ singlecell/tests/testthat/test_identifiability.R
@@ -10,12 +10,12 @@ test_that(".identification works", {
 test_that(".identification is correct", {
   set.seed(20)
   cov_x <- cov(MASS::mvrnorm(n = 10, rep(0, 5), diag(5)))
-  cov_y <- cov(MASS::mvrnorm(n = 10, rep(0, 5), 2*diag(5)))
+  cov_y <- cov(MASS::mvrnorm(n = 10, rep(0, 5), toeplitz(5:1)))
 
   res <- .identification(cov_x, cov_y)
 
-  cov_x <- res %*% cov_x %*% t(res)
-  cov_y <- solve(t(res)) %*% cov_y %*% solve(res)
+  cov_x2 <- res %*% cov_x %*% t(res)
+  cov_y2 <- solve(t(res)) %*% cov_y %*% solve(res)
 
-  expect_true(sum(abs(cov_x - cov_y)) <= 1e-6)
+  expect_true(sum(abs(cov_x2 - cov_y2)) <= 1e-6)
 })
