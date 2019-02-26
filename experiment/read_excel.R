cb4b57781b3c5a3cd30d56505fd65102be248a07
klin1234@gmail.com <klin1234@gmail.com>
2019-02-26 11:45:35 -0500

    Experimenting with using the preselected genes

---
+++ experiment/read_excel.R
@@ -0,0 +1,8 @@
+library(readxl)
+gene_mat <- read_excel("../../raw_data/Marques_genes.xlsx", range = "B3:Y53")
+gene_mat <- as.matrix(gene_mat)
+gene_vec <- sort(unique(as.vector(gene_mat)))
+
+load("../../raw_data/marques.RData")
+gene_all <- colnames(marques$counts)
+length(which(gene_vec %in% gene_all))
