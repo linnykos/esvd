49b5c3f4d296ab496d075a86b807fa75ea0069df
klin1234@gmail.com <klin1234@gmail.com>
2019-02-26 11:59:39 -0500

    Fixing typo

--- main/step0_screening.R
+++ main/step0_screening.R
@@ -15,7 +15,7 @@ dat <- dat[cell_idx,]
 dim(dat)
 
 # use the predetermined set of genes
-gene_mat <- read_excel("../../raw_data/Marques_genes.xlsx", range = "B3:Y53")
+gene_mat <- readxl::read_excel("../../raw_data/Marques_genes.xlsx", range = "B3:Y53")
 gene_mat <- as.matrix(gene_mat)
 gene_vec <- sort(unique(as.vector(gene_mat)))
 gene_idx <- which(colnames(dat) %in% gene_vec)
