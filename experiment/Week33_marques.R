d3f7fb5ea0a5d1ecc38e4ac9634ed97be0af10c8
klin1234@gmail.com <klin1234@gmail.com>
2019-03-11 16:35:21 -0400

    Added the viper experiment to try dry run

---
+++ experiment/Week33_marques.R
@@ -0,0 +1,24 @@
+rm(list=ls())
+set.seed(10)
+library(descend)
+load("../../raw_data/marques.RData")
+
+dat <- marques$counts
+
+cell_types <- unique(marques$cell.info$cell.type)
+#cell_types <- cell_types[-which(cell_types %in% c("OPC", "PPR"))]
+
+cell_idx <- unlist(lapply(cell_types, function(x){
+  tmp <- which(marques$cell.info$cell.type == x)
+  sample(tmp, round(length(tmp)/5))
+}))
+dat <- dat[cell_idx,]
+dim(dat)
+
+# use the predetermined set of genes
+gene_mat <- readxl::read_excel("../../raw_data/Marques_genes.xlsx", range = "B3:Y53")
+gene_mat <- as.matrix(gene_mat)
+gene_vec <- sort(unique(as.vector(gene_mat)))
+gene_idx <- which(colnames(dat) %in% gene_vec)
+dat <- dat[,gene_idx]
+dim(dat)
