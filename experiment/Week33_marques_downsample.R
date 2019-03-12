169107e32ef8f4511c345cb60ab8ba5ead034dde
klin1234@gmail.com <klin1234@gmail.com>
2019-03-12 09:36:03 -0400

    Altering script

--- experiment/Week33_marques_downsample.R
+++ experiment/Week33_marques_downsample.R
@@ -15,8 +15,9 @@ dat <- dat[cell_idx,]
 dim(dat)
 
 # use the predetermined set of genes
-gene_mat <- readxl::read_excel("../../raw_data/Marques_genes.xlsx", range = "B3:Y53")
-gene_mat <- as.matrix(gene_mat)
+# gene_mat <- readxl::read_excel("../../raw_data/Marques_genes.xlsx", range = "B3:Y53")
+# gene_mat <- as.matrix(gene_mat)
+gene_mat <- load("../data/marker_genes.Rda")
 gene_vec <- sort(unique(as.vector(gene_mat)))
 gene_idx <- which(colnames(dat) %in% gene_vec)
 dat <- dat[,gene_idx]
