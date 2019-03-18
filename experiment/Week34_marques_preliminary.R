a89d5f8321871ebc05d374d4606a08920f90e194
klin1234@gmail.com <klin1234@gmail.com>
2019-03-18 11:20:52 -0400

    Trying the analysis without logging

---
+++ experiment/Week34_marques_preliminary.R
@@ -0,0 +1,30 @@
+rm(list=ls())
+load("../results/step3_scalar_heuristic.RData")
+plot(quality_vec) # uh oh... not looking great...
+
+rm(list=ls())
+load("../results/step4_factorization.RData")
+cell_type_vec <- as.character(marques$cell.info$cell.type[cell_idx])
+# keep only the first two letters
+cell_type_vec <- as.character(sapply(cell_type_vec, function(x){substr(x,1,2)}))
+col_idx <- as.numeric(as.factor(cell_type_vec))
+
+num_cell <- length(unique(cell_type_vec))
+i1 <- 1; i2 <- 3
+xlim <- range(res_our$u_mat[,i1])
+ylim <- range(res_our$u_mat[,i2])
+par(mfrow = c(2,3))
+for(i in 1:6){
+  idx <- which(col_idx == i)
+  plot(res_our$u_mat[idx,i1], res_our$u_mat[idx,i2], asp = T, pch = 16,
+       col = col_idx[idx], xlim = xlim, ylim = ylim)
+
+  lines(c(-100,100), rep(0,2), lwd = 2, lty = 2)
+  lines(rep(0,2), c(-100,100), lwd = 2, lty = 2)
+}
+
+#all in the same plot
+plot(res_our$u_mat[,1], res_our$u_mat[,2], asp = T, pch = 16,
+     col = col_idx)
+
+
