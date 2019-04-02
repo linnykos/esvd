be4e78d63acb59e665aee5c3462e902b489b4e75
klin1234@gmail.com <klin1234@gmail.com>
2019-04-02 11:39:53 -0400

    typo

--- experiment/Week36_marques_our.R
+++ experiment/Week36_marques_our.R
@@ -22,46 +22,47 @@ for(i in 1:length(cluster_group_list)){
 # run slingshot
 curves <- slingshot(dat/reduction_factor, cluster_labels, starting_cluster = cluster_group_list[[1]][1], cluster_group_list = cluster_group_list, verbose = T,
                     b = 1, upscale_vec = upscale_vec)
+save.image("tmp.RData")
 
-
-dat2 <- dat/reduction_factor
-combn_mat <- utils::combn(d, 2)
-range_mat <- apply(dat2, 2, range)
-col_vec <- numeric(length(unique(cluster_labels)))
-alpha_val <- 1
-for(i in 1:3){
-  col_vec[cluster_group_list[[i]]] <- rgb(238/255,204/255,17/255,alpha_val)
-}
-col_vec[cluster_group_list[[4]]] <- rgb(129/255,199/255,124/255,alpha_val)
-col_vec[cluster_group_list[[5]]] <- rgb(227/255,73/255,86/255,alpha_val)
-col_vec[cluster_group_list[[6]]] <- rgb(100/255,140/255,252/255,alpha_val)
-cluster_center <- .compute_cluster_center(dat2, .construct_cluster_matrix(cluster_labels))
-
-png("../figure/experiment/Week36_marques_our_lineage.png", height = length(curves$curves)*2000/2.5, width = 2000, res = 300, units = "px")
-par(mfrow = c(length(curves$curves), ncol(combn_mat)), mar = c(4,4,4,0.5))
-for(k in 1:length(curves$curves)){
-  for(i in 1:ncol(combn_mat)){
-    cell_idx <- which(cluster_labels %in% curves$lineages[[k]])
-
-    idx1 <- combn_mat[1,i]; idx2 <- combn_mat[2,i]
-    plot(dat2[cell_idx,idx1], dat2[cell_idx,idx2], pch = 16, col = rgb(0.85,0.85,0.85,1),
-         asp = T, cex = 1,
-         xlim = range_mat[,idx1], ylim = range_mat[,idx2],
-         xlab = paste0("Latent dimension ", idx1),
-         ylab = paste0("Latent dimension ", idx2),
-         main = ifelse(i == 2, paste0("Our Lineage ", k), ""))
-
-    # plot curves
-    ord <- curves$curves[[k]]$ord
-    lines(curves$curves[[k]]$s[ord, idx1], curves$curves[[k]]$s[ord, idx2], lwd = 3.5,
-          col = "white")
-    lines(curves$curves[[k]]$s[ord, idx1], curves$curves[[k]]$s[ord, idx2], lwd = 3,
-          col = "black")
-
-    # plot points
-    points(cluster_center[curves$lineages[[k]], idx1],
-           cluster_center[curves$lineages[[k]], idx2], pch = 16,
-           col = col_vec[curves$lineages[[k]]], cex = 2)
-  }
-}
-graphics.off()
+#
+# dat2 <- dat/reduction_factor
+# combn_mat <- utils::combn(d, 2)
+# range_mat <- apply(dat2, 2, range)
+# col_vec <- numeric(length(unique(cluster_labels)))
+# alpha_val <- 1
+# for(i in 1:3){
+#   col_vec[cluster_group_list[[i]]] <- rgb(238/255,204/255,17/255,alpha_val)
+# }
+# col_vec[cluster_group_list[[4]]] <- rgb(129/255,199/255,124/255,alpha_val)
+# col_vec[cluster_group_list[[5]]] <- rgb(227/255,73/255,86/255,alpha_val)
+# col_vec[cluster_group_list[[6]]] <- rgb(100/255,140/255,252/255,alpha_val)
+# cluster_center <- .compute_cluster_center(dat2, .construct_cluster_matrix(cluster_labels))
+#
+# png("../figure/experiment/Week36_marques_our_lineage.png", height = length(curves$curves)*2000/2.5, width = 2000, res = 300, units = "px")
+# par(mfrow = c(length(curves$curves), ncol(combn_mat)), mar = c(4,4,4,0.5))
+# for(k in 1:length(curves$curves)){
+#   for(i in 1:ncol(combn_mat)){
+#     cell_idx <- which(cluster_labels %in% curves$lineages[[k]])
+#
+#     idx1 <- combn_mat[1,i]; idx2 <- combn_mat[2,i]
+#     plot(dat2[cell_idx,idx1], dat2[cell_idx,idx2], pch = 16, col = rgb(0.85,0.85,0.85,1),
+#          asp = T, cex = 1,
+#          xlim = range_mat[,idx1], ylim = range_mat[,idx2],
+#          xlab = paste0("Latent dimension ", idx1),
+#          ylab = paste0("Latent dimension ", idx2),
+#          main = ifelse(i == 2, paste0("Our Lineage ", k), ""))
+#
+#     # plot curves
+#     ord <- curves$curves[[k]]$ord
+#     lines(curves$curves[[k]]$s[ord, idx1], curves$curves[[k]]$s[ord, idx2], lwd = 3.5,
+#           col = "white")
+#     lines(curves$curves[[k]]$s[ord, idx1], curves$curves[[k]]$s[ord, idx2], lwd = 3,
+#           col = "black")
+#
+#     # plot points
+#     points(cluster_center[curves$lineages[[k]], idx1],
+#            cluster_center[curves$lineages[[k]], idx2], pch = 16,
+#            col = col_vec[curves$lineages[[k]]], cex = 2)
+#   }
+# }
+# graphics.off()
