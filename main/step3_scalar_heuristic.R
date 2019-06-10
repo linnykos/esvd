99cd463859964c8460ad522c58af7164557bd584
klin1234@gmail.com <klin1234@gmail.com>
2019-06-10 14:47:49 -0400

    Fixed minor bug that caused the crash

--- main/step3_scalar_heuristic.R
+++ main/step3_scalar_heuristic.R
@@ -1,7 +1,7 @@
 set.seed(10)
 load(paste0("../results/step2_naive_svd", suffix, ".RData"))
 
-max_val <- 5000
+max_val <- -5000
 scalar_vec <- c(0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5)
 res_list <- vector("list", length(scalar_vec))
 
