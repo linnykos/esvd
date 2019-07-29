67ac23b570bf6aade1076d3b4767e61404555cde
Kevin Lin <kevinlin@Kevins-MBP.fios-router.home>
2019-07-28 23:09:02 -0400

    Updated all the figures

--- main/step6_figures.R
+++ main/step6_figures.R
@@ -6,7 +6,8 @@ color_func <- function(alpha = 0.2){
     rgb(86/255, 180/255, 233/255, alpha), #skyblue
     rgb(0/255, 158/255, 115/255, alpha), #bluish green
     rgb(0/255, 114/255, 178/255,alpha), #blue
-    rgb(230/255, 159/255, 0/255,alpha)) #orange
+    rgb(230/255, 159/255, 0/255,alpha), #orange
+    rgb(210/255, 198/255, 36/255, alpha)) #darker yellow
 }
 cell_type_vec <- as.character(marques$cell.info$cell.type[cell_idx])
 cell_type_vec <- as.factor(cell_type_vec)
@@ -207,17 +208,20 @@ graphics.off()
 cluster_center <- .compute_cluster_center(res_our$u_mat[,1:3], .construct_cluster_matrix(cluster_labels))
 custom_cluster_group_list <- list(13, 12, 1, c(10,11), c(2,3), c(4:7), c(8,9))
 
-num_order_vec <- c(1, rep(3,3), rep(5,3), rep(4,2), rep(2,2),  rep(1,2))
+num_order_vec <- c(5, rep(3,2), 3, rep(1,3), rep(4,2), rep(2,2),  rep(5,2))
 col_vec <- color_func(1)[num_order_vec]
 col_vec2 <- color_func(0.1)[num_order_vec]
 col_vec3 <- color_func(0.3)[num_order_vec]
-col_name <- c("yellow", rep("bluish green", 3), rep("orange", 3), rep("blue", 2), rep("skyblue", 2), rep("yellow", 2))
+col_name <- c("orange", rep("bluish green", 3), rep("yellow", 3), rep("blue", 2), rep("skyblue", 2), rep("orange", 2))
 col_info <- data.frame(name = levels(cell_type_vec),
                        idx = sort(unique(cluster_labels)),
                        level = sapply(1:13, function(y){which(sapply(custom_cluster_group_list, function(x){y %in% x}))}),
                        col_name = col_name,
                        col_code = col_vec)
 col_info
+
+col_vec_short <- color_func(0.9)[c(6,4)]
+
 
 png(paste0("../figure/main/eSVD_trajectory_2d.png"),
     height = 1000, width = 2700, res = 300, units = "px")
@@ -240,92 +244,11 @@ for(k in 1:ncol(combn_mat)){
   curves <- our_curves$curves
   for(ll in 1:length(curves)){
     ord <- curves[[ll]]$ord
-    lines(x = curves[[ll]]$s[ord, i], y = curves[[ll]]$s[ord, j], col = "white", lwd = 7)
+    lines(x = curves[[ll]]$s[ord, i], y = curves[[ll]]$s[ord, j], col = "white", lwd = 8)
     lines(x = curves[[ll]]$s[ord, i], y = curves[[ll]]$s[ord, j], col = col_vec_short[ll], lwd = 5)
     lines(x = curves[[ll]]$s[ord, i], y = curves[[ll]]$s[ord, j], col = "black",
           lty = 3, lwd = 2)
   }
-}
-graphics.off()
-
-# 3D plots
-
-png(paste0("../figure/main/eSVD_theta45_phi225.png"),
-    height = 2000, width = 2000, res = 300, units = "px")
-par(mar = c(0,0,4,0))
-slingshot_3dplot(res_our$u_mat[,1:3], cluster_labels,
-                 bg_col_vec = col_vec2,
-                 breaks = seq(0.5, 13.5, by = 1),
-                 cluster_center = cluster_center,
-                 center_col_vec = col_vec,
-                 center_labels = 1:13,
-                 curves = NA,
-                 pch = 16, main = "eSVD embedding and trajectories\n(Curved Gaussian)",
-                 xlab = "", ylab = "",
-                 zlab = "Latent dimension 3",
-                 theta = 45, phi = 225,
-                 xlim = c(-2.45, 0), ylim = c(-1.5,1), zlim = c(-1,0.75))
-
-curves <- our_curves$curves
-col_vec_short <- color_func(0.9)[c(5,4)]
-for(i in 1:length(curves)){
-  ord <- curves[[i]]$ord
-  plot3D::lines3D(x = curves[[i]]$s[ord, 1],
-                  y = curves[[i]]$s[ord, 2],
-                  z = curves[[i]]$s[ord, 3],
-                  add = T, colkey = F, col = "black", lwd = 2)
-  plot3D::lines3D(x = curves[[i]]$s[ord, 1],
-                  y = curves[[i]]$s[ord, 2],
-                  z = curves[[i]]$s[ord, 3],
-                  add = T, colkey = F, col = col_vec_short[i], lwd = 2)
-}
-graphics.off()
-
-# 3D plots with tubes
-
-our_tube_list <- lapply(1:length(our_curves$curves), function(x){
-  s_mat <- our_curves$curves[[x]]$s[our_curves$curves[[x]]$ord,]
-  construct_3d_tube(s_mat, radius = our_sd_val$sd_val)
-})
-
-png(paste0("../figure/main/eSVD_theta45_phi225_tube.png"),
-    height = 2000, width = 2000, res = 300, units = "px")
-par(mar = c(0,0,4,0))
-slingshot_3dplot(res_our$u_mat[,1:3], cluster_labels,
-                 bg_col_vec = col_vec2,
-                 breaks = seq(0.5, 13.5, by = 1),
-                 cluster_center = cluster_center,
-                 center_col_vec = col_vec,
-                 center_labels = 1:13,
-                 curves = NA,
-                 pch = 16, main = "eSVD embedding with uncertainty tubes\n(Curved Gaussian)",
-                 xlab = "", ylab = "",
-                 zlab = "Latent dimension 3",
-                 theta = 45, phi = 225,
-                 xlim = c(-2.45, 0), ylim = c(-1.5,1), zlim = c(-1,0.75))
-
-curves <- our_curves$curves
-col_vec_short <- color_func(0.9)[c(5,4)]
-for(i in 1:length(curves)){
-  ord <- curves[[i]]$ord
-  plot3D::lines3D(x = curves[[i]]$s[ord, 1],
-                  y = curves[[i]]$s[ord, 2],
-                  z = curves[[i]]$s[ord, 3],
-                  add = T, colkey = F, col = "black", lwd = 2)
-  plot3D::lines3D(x = curves[[i]]$s[ord, 1],
-                  y = curves[[i]]$s[ord, 2],
-                  z = curves[[i]]$s[ord, 3],
-                  add = T, colkey = F, col = col_vec_short[i], lwd = 2)
-
-  col_mat <- our_tube_list[[i]]$z_mat
-
-  plot3D::surf3D(our_tube_list[[i]]$x_mat,
-                 our_tube_list[[i]]$y_mat,
-                 our_tube_list[[i]]$z_mat, add = T,
-                 colvar = col_mat,
-                 col = colorRampPalette(c("white", col_vec_short[i]))(100),
-                 breaks = seq(min(col_mat), max(col_mat), length.out = 101),
-                 colkey = F)
 }
 graphics.off()
 
@@ -337,10 +260,10 @@ svd_u <- svd_res$u[,1:p] %*% diag(sqrt(svd_res$d[1:p]))
 
 cluster_center <- .compute_cluster_center(svd_u[,1:3], .construct_cluster_matrix(cluster_labels))
 
-num_order_vec <- c(1, rep(3,2), rep(5,6), rep(2,2),  rep(1,2))
+num_order_vec <- c(5, rep(3,2), rep(1,6), rep(2,2),  rep(5,2))
 col_vec <- color_func(1)[num_order_vec]
 col_vec3 <- color_func(0.3)[num_order_vec]
-col_name <- c("yellow", rep("bluish green", 2), rep("orange", 6), rep("skyblue", 2), rep("yellow", 2))
+col_name <- c("orange", rep("bluish green", 2), rep("yellow", 6), rep("skyblue", 2), rep("orange", 2))
 col_info <- data.frame(name = levels(cell_type_vec),
                        idx = sort(unique(cluster_labels)),
                        level = sapply(1:13, function(y){which(sapply(cluster_group_list, function(x){y %in% x}))}),
@@ -375,79 +298,5 @@ for(k in 1:ncol(combn_mat)){
     lines(x = curves[[ll]]$s[ord, i], y = curves[[ll]]$s[ord, j], col = "black", lwd = 1.5)
   }
 }
-graphics.off()
-
-
-# svd 3D plots
-
-png(paste0("../figure/main/SVD_theta135_phi180.png"),
-    height = 2000, width = 2000, res = 300, units = "px")
-par(mar = c(1,1,4,1))
-slingshot_3dplot(svd_u[,1:3], cluster_labels,
-                 bg_col_vec = col_vec2,
-                 breaks = seq(0.5, 13.5, by = 1),
-                 cluster_center = cluster_center,
-                 center_col_vec = col_vec,
-                 center_labels = 1:13,
-                 curves = NA,
-                 pch = 16, main = "SVD embedding and trajectories\n(Constant-variance Gaussian)",
-                 xlab = "", ylab = "Latent dimension 2",
-                 zlab = "Latent dimension 3",
-                 theta = 135, phi = 180)
-
-curves <- naive_curves$curves
-col_vec_short <- color_func(0.9)[c(5,4)]
-for(i in 1:length(curves)){
-  ord <- curves[[i]]$ord
-  plot3D::lines3D(x = curves[[i]]$s[ord, 1],
-                  y = curves[[i]]$s[ord, 2],
-                  z = curves[[i]]$s[ord, 3],
-                  add = T, colkey = F, col = "black", lwd = 2)
-}
-graphics.off()
-
-# svd 3D plots with tubes
-
-naive_tube_list <- lapply(1:length(naive_curves$curves), function(x){
-  s_mat <- naive_curves$curves[[x]]$s[naive_curves$curves[[x]]$ord,]
-  construct_3d_tube(s_mat, radius = naive_sd_val$sd_val)
-})
-col_vec_short <- color_func(0.9)[c(4)]
-
-png(paste0("../figure/main/SVD_theta135_phi180_tube.png"),
-    height = 2000, width = 2000, res = 300, units = "px")
-par(mar = c(1,1,4,1))
-slingshot_3dplot(svd_u[,1:3], cluster_labels,
-                 bg_col_vec = col_vec2,
-                 breaks = seq(0.5, 13.5, by = 1),
-                 cluster_center = cluster_center,
-                 center_col_vec = col_vec,
-                 center_labels = 1:13,
-                 curves = NA,
-                 pch = 16, main = "SVD embedding and trajectories\n(Constant-variance Gaussian)",
-                 xlab = "", ylab = "Latent dimension 2",
-                 zlab = "Latent dimension 3",
-                 theta = 135, phi = 180)
-
-curves <- naive_curves$curves
-col_vec_short <- color_func(0.9)[c(4)]
-for(i in 1:length(curves)){
-  ord <- curves[[i]]$ord
-  plot3D::lines3D(x = curves[[i]]$s[ord, 1],
-                  y = curves[[i]]$s[ord, 2],
-                  z = curves[[i]]$s[ord, 3],
-                  add = T, colkey = F, col = "black", lwd = 2)
-}
-
-i <- 3
-col_mat <- naive_tube_list[[i]]$z_mat
-
-plot3D::surf3D(naive_tube_list[[i]]$x_mat,
-               naive_tube_list[[i]]$y_mat,
-               naive_tube_list[[i]]$z_mat, add = T,
-               colvar = col_mat,
-               col = colorRampPalette(c("white", col_vec_short[1]))(100),
-               breaks = seq(min(col_mat), max(col_mat), length.out = 101),
-               colkey = F)
 graphics.off()
 
--- simulation/wasserstein_simulation_plot.R
+++ simulation/wasserstein_simulation_plot.R
@@ -7,61 +7,40 @@ plot(tmp[,1], tmp[,2], asp = T, col = rep(1:4, each = nrow(tmp)/4), pch = 16)
 
 #####################
 
-# zz <- res[[10]][[1]]$u_mat %*% t(res[[10]][[1]]$v_mat)
-# tmpu <- svd(zz); tmpu <- (nrow(res[[10]][[1]]$u_mat)/nrow(res[[10]][[1]]$v_mat))^(1/4)*tmpu$u %*% diag(sqrt(tmpu$d))
-# eigen_val <- eigen(stats::cov(tmpu))$values
-# #only 2 eigenvalues
-# eigen_val <- eigen_val[1:2]
-# range(log(eigen_val)/(-log(c(1.01,2))))
+# collect the values
+loss_mat <- matrix(0, nrow = 2*nrow(paramMat), ncol = trials)
+for(i in 1:nrow(paramMat)){
+  for(j in 1:trials){
+    loss_mat[(i-1)+1,j] <- res[[i]][[j]]$l2_loss
+    loss_mat[2*i,j] <- res[[i]][[j]]$wasserstein_loss
+  }
+}
 
-#forbenius_loss <- sapply(1:length(res), function(i){
-loss_mat <- lapply(1:length(res), function(i){
-  print(i)
-  sapply(1:trials, function(y){
-    if(length(res[[i]][[y]]) == 1) return(rep(NA,2))
-    cat('*')
-    set.seed(y)
-    vec <- paramMat[i,]
-    obj <- .data_generator(cell_pop, gene_pop, n_each = vec["n"], d_each = vec["d"])
-    n <- nrow(obj$cell_mat)
-    forbenius <- .l2norm(obj$cell_mat - res[[i]][[y]]$u_mat)^2/n
-    wasserstein <- transport::wasserstein(transport::pp(obj$cell_mat),
-                                          transport::pp(res[[i]][[y]]$u_mat), p = 1)
+l2_val <- matrix(0, nrow = nrow(paramMat), ncol = 3)
+for(i in 1:nrow(paramMat)){
+  #mean_val <- median(loss_mat[(i-1)+1,])
+  #sd_val <- sd(loss_mat[(i-1)+1,])
+  #l2_val[i,] <- c(mean_val-sd_val, mean_val, mean_val+sd_val)
+  l2_val[i,] <- c(quantile(loss_mat[(i-1)+1,], probs = 0.25),
+                  quantile(loss_mat[(i-1)+1,], probs = 0.5),
+                  quantile(loss_mat[(i-1)+1,], probs = 0.75))
+}
 
-    c(forbenius, wasserstein)
-  })
-})
-
-save.image("../results/wasserstein_simulation_values.RData")
-
-forbenius_bound <- sapply(loss_mat, function(x){
-  stats::quantile(x[1,], na.rm = T, probs = c(0.25, 0.5, 0.75))
-})
-
-wasserstein_bound <- sapply(loss_mat, function(x){
-  stats::quantile(x[2,], na.rm = T,probs = c(0.25, 0.5, 0.75))
-})
+wass_val <- matrix(0, nrow = nrow(paramMat), ncol = 3)
+for(i in 1:nrow(paramMat)){
+  mean_val <- median(loss_mat[2*i,])
+  sd_val <- sd(loss_mat[2*i,])
+  wass_val[i,] <- c(mean_val-sd_val, mean_val, mean_val+sd_val)
+}
 
 #########################
 
-# figure out hypothetical bounds
-vec1 <- forbenius_bound[2,]
-vec2 <- 4*paramMat[,"n"]
-range(-log(vec1)/log(vec2))
-
-png("../figure/simulation/wasserstein.png", height = 1100, width = 2400, res = 300, units = "px")
-par(mfrow = c(1,2), mar = c(4,4,4,0.5))
-plot(paramMat[,"n"]*4, forbenius_bound[2,], ylim = range(forbenius_bound),
+keep_idx <- 1:9
+png("../figure/simulation/wasserstein.png", height = 1200, width = 1500, res = 300, units = "px")
+plot(paramMat[keep_idx,"n_each"]*4, l2_val[keep_idx,2], ylim = range(l2_val[keep_idx,]),
      pch = 16, xlab = "n", ylab = "Forbenius loss",
-     main = "Forbenius loss for cells")
-lines(paramMat[,"n"]*4, forbenius_bound[2,], lwd = 2)
-Hmisc::errbar(paramMat[,"n"]*4, forbenius_bound[2,], yplus = forbenius_bound[3,],
-              yminus = forbenius_bound[1,], add = T)
-
-plot(paramMat[,"n"]*4, wasserstein_bound[2,], ylim = range(wasserstein_bound),
-     pch = 16, xlab = "n", ylab = "Wasserstein loss",
-     main = "Wasserstein loss for cells")
-lines(paramMat[,"n"]*4, wasserstein_bound[2,], lwd = 2)
-Hmisc::errbar(paramMat[,"n"]*4, wasserstein_bound[2,], yplus = wasserstein_bound[3,],
-              yminus = wasserstein_bound[1,], add = T)
+     main = "Forbenius loss (squared) for cells")
+lines(paramMat[keep_idx,"n_each"]*4, l2_val[keep_idx,2], lwd = 2)
+Hmisc::errbar(paramMat[keep_idx,"n_each"]*4, l2_val[keep_idx,2], yplus = l2_val[keep_idx,3],
+              yminus = l2_val[keep_idx,1], add = T)
 graphics.off()
