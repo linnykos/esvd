b24ffff249f1df365b651a655ff131ff1d5d7f99
Kevin Lin <klin1234@gmail.com>
2019-08-21 11:51:42 -0400

    Removed all the experiments (moved them to a different branch)

--- experiment/descend_experiment.R
+++
@@ -1,9 +0,0 @@
-library(descend)
-data(zeisel)
-
-result <- runDescend(zeisel$count.matrix.small,
-                     scaling.consts = zeisel$library.size, n.cores = 1)
-
-hvg <- findHVG(result)
-
-which(hvg$score.mat[,2]>2)
