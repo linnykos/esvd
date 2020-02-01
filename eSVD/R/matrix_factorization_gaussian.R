c9bcd21c28131dd8dfd9c08d4065946dc5e35d7e
Kevin Lin <klin1234@gmail.com>
2020-02-01 18:45:33 -0500

    Starting to reformt probability distribution

--- eSVD/R/matrix_factorization_gaussian.R
+++ eSVD/R/matrix_factorization_gaussian.R
@@ -1,9 +1,9 @@
-# distribution: one-parameter Gaussian where var = mean/2
+# distribution: one-parameter Gaussian where var = mean/scalar
 # natural parameter: m_{ij} = u_i^Tv_j
-# relation to canonical parameters: -2/mu^2 and m_{ij} = 4/mu
-# optimization problem: -log(m_{ij}) - a_{ij}^2*(-m_{ij}^2/8) - a_{ij}*m_{ij}
+# relation to canonical parameters: m_{ij} = 1/mu_{ij}
+# optimization problem: -log(m_{ij}) - scalar^2*a_{ij}^2*(-m_{ij}^2)/n - scalar^2*a_{ij}*m_{ij}
 
-.evaluate_objective.gaussian <- function(dat, u_mat, v_mat, scalar = 2, ...){
+.evaluate_objective.curved_gaussian <- function(dat, u_mat, v_mat, scalar = 2, ...){
   n <- nrow(dat); p <- ncol(dat)
   pred_mat <- u_mat %*% t(v_mat)
   idx <- which(!is.na(dat))
@@ -15,7 +15,7 @@
         pred_mat[idx]^2*dat[idx]^2*scalar^2/2)
 }
 
-.evaluate_objective_single.gaussian <- function(dat_vec, current_vec, other_mat, n, p,
+.evaluate_objective_single.curved_gaussian <- function(dat_vec, current_vec, other_mat, n, p,
                                                 scalar = 2, ...){
   pred_vec <- other_mat %*% current_vec
   idx <- which(!is.na(dat_vec))
@@ -27,7 +27,7 @@
         pred_vec[idx]^2*dat_vec[idx]^2*scalar^2/2)
 }
 
-.gradient_vec.gaussian <- function(dat_vec, current_vec, other_mat, n, p, scalar = 2, ...){
+.gradient_vec.curved_gaussian <- function(dat_vec, current_vec, other_mat, n, p, scalar = 2, ...){
   pred_vec <- other_mat %*% current_vec
   idx <- which(!is.na(dat_vec))
   stopifnot(all(pred_vec < 0))
@@ -41,7 +41,7 @@
   if(is.matrix(tmp)) 1/(n*p) * rowSums(tmp) else 1/(n*p) * sum(tmp)
 }
 
-.evaluate_objective_mat.gaussian <- function(dat, pred_mat, scalar = 2, ...){
+.evaluate_objective_mat.curved_gaussian <- function(dat, pred_mat, scalar = 2, ...){
   n <- nrow(dat); p <- ncol(dat)
   stopifnot(all(pred_mat < 0))
   idx <- which(!is.na(dat))
@@ -52,7 +52,7 @@
         pred_mat[idx]^2*dat[idx]^2*scalar^2/2)
 }
 
-.gradient_mat.gaussian <- function(dat, pred_mat, scalar = 2, ...){
+.gradient_mat.curved_gaussian <- function(dat, pred_mat, scalar = 2, ...){
   n <- nrow(dat); p <- ncol(dat)
   stopifnot(all(!is.na(dat)))
   stopifnot(all(pred_mat < 0))
