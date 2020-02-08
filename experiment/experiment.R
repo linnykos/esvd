d539d425110e8d891b15459ab99a02bc4045c7c7
Kevin Lin <klin1234@gmail.com>
2020-02-08 02:09:19 -0500

    About to completely revamp reparameterize since I think I don't need to use the full dim matrix

--- experiment/experiment.R
+++ experiment/experiment.R
@@ -1,27 +1,38 @@
 rm(list=ls())
-set.seed(10)
-dat <- matrix(rexp(100), nrow = 10, ncol = 10)
+n <- 20
+x = 7
 
-dat2 <- dat; dat2[2,1] <- NA
-dat3 <- dat
-for(i in 1:nrow(dat3)){
-  dat3[i, sample(1:ncol(dat3), 2)] <- NA
+set.seed(x)
+u_mat <- matrix(abs(rnorm(n)), ncol = 1)
+v_mat <- -matrix(abs(rnorm(n)), ncol = 1)
+pred_mat <- u_mat %*% t(v_mat)
+
+dat <- pred_mat
+
+for(i in 1:n){
+  for(j in 1:n){
+    dat[i,j] <- stats::rexp(1, rate = -pred_mat[i,j])
+  }
 }
 
-init <- initialization(dat, max_val = 100, family = "curved_gaussian")
+class(dat) <- c("exponential", class(dat)[length(class(dat))])
 
-range(init$u_mat %*% t(init$v_mat))
 
-# res <- fit_factorization(dat, u_mat = init$u_mat, v_mat = init$v_mat,
-#                          max_val = 100, max_iter = 100, family = "curved_gaussian")
 
-##################
+init <- initialization(dat, k = 1, family = "exponential", max_val = -100)
+
+# fit <- fit_factorization(dat, k = 1, u_mat = init$u_mat, v_mat = init$v_mat,
+#                          max_iter = 10, max_val = -100,
+#                          family = "exponential")
+
+####################
+
+k = 1
 u_mat = init$u_mat
 v_mat = init$v_mat
-max_val = 100
-max_iter = 100
-family = "curved_gaussian"
-
+max_iter = 10
+max_val = -100
+family = "exponential"
 reparameterize = T
 tol = 1e-3
 verbose = F
@@ -47,10 +58,7 @@ obj_vec <- c(next_obj)
 if(verbose) print(paste0("Finished initialization : Current objective is ", next_obj))
 if(return_path) res_list <- list(list(u_mat = u_mat, v_mat = v_mat)) else res_list <- NA
 
-
-#while((is.na(tol) | abs(current_obj - next_obj) > tol) & length(obj_vec) < max_iter){
-  print(length(obj_vec))
-  print(range(u_mat %*% t(v_mat)))
+while((is.na(tol) | abs(current_obj - next_obj) > tol) & length(obj_vec) < 3){
   current_obj <- next_obj
 
   pred_mat <- u_mat%*%t(v_mat)
@@ -62,7 +70,6 @@ if(return_path) res_list <- list(list(u_mat = u_mat, v_mat = v_mat)) else res_li
     svd_res <- .svd_projection(pred_mat, k = k, factors = T)
     u_mat <- svd_res$u_mat; v_mat <- svd_res$v_mat
   }
-
   print(range(u_mat %*% t(v_mat)))
 
   u_mat <- .optimize_mat(dat, u_mat, v_mat, left = T, max_val = max_val, parallelized = !is.na(cores))
@@ -74,56 +81,39 @@ if(return_path) res_list <- list(list(u_mat = u_mat, v_mat = v_mat)) else res_li
   } else { stopifnot(all(pred_mat >= 0)) }
   }
 
-  print(range(u_mat %*% t(v_mat)))
 
   if(reparameterize){
     svd_res <- .svd_projection(pred_mat, k = k, factors = T)
     u_mat <- svd_res$u_mat; v_mat <- svd_res$v_mat
   }
-
-  print(paste0("? ", range(u_mat %*% t(v_mat))))
+  print(range(u_mat %*% t(v_mat)))
 
   v_mat <- .optimize_mat(dat, v_mat, u_mat, left = F, max_val = max_val, parallelized = !is.na(cores))
 
-  print(range(u_mat %*% t(v_mat)))
-
   next_obj <- .evaluate_objective(dat, u_mat, v_mat)
 
-
+  print(range(u_mat %*% t(v_mat)))
   if(verbose) print(paste0("Iter ", length(obj_vec), ": Decrease is ", current_obj - next_obj))
   if(return_path) res_list[[length(res_list)+1]] <- list(u_mat = u_mat, v_mat = v_mat)
 
   obj_vec <- c(obj_vec, next_obj)
-#}
+}
 
-  print(length(obj_vec))
-  print(range(u_mat %*% t(v_mat)))
-  current_obj <- next_obj
+current_obj <- next_obj
 
-  pred_mat <- u_mat%*%t(v_mat)
-  if(!is.na(direction)){ if(direction == "<=") { stopifnot(all(pred_mat <= 0))
-  } else { stopifnot(all(pred_mat >= 0)) }
-  }
+pred_mat <- u_mat%*%t(v_mat)
+if(!is.na(direction)){ if(direction == "<=") { stopifnot(all(pred_mat <= 0))
+} else { stopifnot(all(pred_mat >= 0)) }
+}
 
-  if(reparameterize){
-    svd_res <- .svd_projection(pred_mat, k = k, factors = T)
-    u_mat <- svd_res$u_mat; v_mat <- svd_res$v_mat
-  }
-
-  print(range(u_mat %*% t(v_mat)))
-
-  u_mat <- .optimize_mat(dat, u_mat, v_mat, left = T, max_val = max_val, parallelized = !is.na(cores))
-
-  print(range(u_mat %*% t(v_mat)))
-
-  pred_mat <- u_mat%*%t(v_mat)
-  if(!is.na(direction)){ if(direction == "<=") { stopifnot(all(pred_mat <= 0))
-  } else { stopifnot(all(pred_mat >= 0)) }
-  }
-
-  print(range(u_mat %*% t(v_mat)))
+print(range(u_mat %*% t(v_mat)))
+svd_res <- .svd_projection(pred_mat, k = k, factors = T)
+print(range(svd_res$u_mat %*% t(svd_res$v_mat)))
 
 
-mat = u_mat %*% t(v_mat)
-svd_res <- .svd_projection(mat, k = k, factors = T)
-print(paste0("? ", range(svd_res$u_mat %*% t(svd_res$v_mat))))
+
+svd_u <- svd(u_mat)
+svd_v <- svd(v_mat)
+svd_3 <- svd(svd_u$d * t(svd_u$v) %*% svd_v$v * svd_v$d)
+
+
