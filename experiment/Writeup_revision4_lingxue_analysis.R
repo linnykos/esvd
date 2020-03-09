1793c794b2368b54fd846d9874b61c9b6ef2a2e0
Kevin Lin <klin1234@gmail.com>
2020-03-09 11:30:57 -0400

    More minor adjustments

--- experiment/Writeup_revision4_lingxue_analysis.R
+++ experiment/Writeup_revision4_lingxue_analysis.R
@@ -1,23 +1,23 @@
 rm(list=ls())
-#
-# labels_file_vec <- c("/raid6/Kevin/singlecell_data/Cleaned/Baron_human1/Baron_human1_label.csv",
-#                      "/raid6/Kevin/singlecell_data/Cleaned/Baron_human2/Baron_human2_label.csv",
-#                      "/raid6/Kevin/singlecell_data/Cleaned/Baron_human3/Baron_human3_label.csv",
-#                      "/raid6/Kevin/singlecell_data/Cleaned/Baron_human4/Baron_human4_label.csv",
-#                      "/raid6/Kevin/singlecell_data/Cleaned/Baron_mouse1/Baron_mouse1_label.csv",
-#                      "/raid6/Kevin/singlecell_data/Cleaned/Baron_mouse2/Baron_mouse2_label.csv",
-#                      "/raid6/Kevin/singlecell_data/Cleaned/Darmanis/Darmanis_label.csv")
-# dat_file_vec <- c("/raid6/Kevin/singlecell_data/Cleaned/Baron_human1/Baron_human1_expr.csv",
-#                   "/raid6/Kevin/singlecell_data/Cleaned/Baron_human2/Baron_human2_expr.csv",
-#                   "/raid6/Kevin/singlecell_data/Cleaned/Baron_human3/Baron_human3_expr.csv",
-#                   "/raid6/Kevin/singlecell_data/Cleaned/Baron_human4/Baron_human4_expr.csv",
-#                   "/raid6/Kevin/singlecell_data/Cleaned/Baron_mouse1/Baron_mouse1_expr.csv",
-#                   "/raid6/Kevin/singlecell_data/Cleaned/Baron_mouse2/Baron_mouse2_expr.csv",
-#                   "/raid6/Kevin/singlecell_data/Cleaned/Darmanis/Darmanis_expr.csv")
-#
-# ncores <- 20
-# doMC::registerDoMC(cores = ncores)
-#
+
+labels_file_vec <- c("/raid6/Kevin/singlecell_data/Cleaned/Baron_human1/Baron_human1_label.csv",
+                     "/raid6/Kevin/singlecell_data/Cleaned/Baron_human2/Baron_human2_label.csv",
+                     "/raid6/Kevin/singlecell_data/Cleaned/Baron_human3/Baron_human3_label.csv",
+                     "/raid6/Kevin/singlecell_data/Cleaned/Baron_human4/Baron_human4_label.csv",
+                     "/raid6/Kevin/singlecell_data/Cleaned/Baron_mouse1/Baron_mouse1_label.csv",
+                     "/raid6/Kevin/singlecell_data/Cleaned/Baron_mouse2/Baron_mouse2_label.csv",
+                     "/raid6/Kevin/singlecell_data/Cleaned/Darmanis/Darmanis_label.csv")
+dat_file_vec <- c("/raid6/Kevin/singlecell_data/Cleaned/Baron_human1/Baron_human1_expr.csv",
+                  "/raid6/Kevin/singlecell_data/Cleaned/Baron_human2/Baron_human2_expr.csv",
+                  "/raid6/Kevin/singlecell_data/Cleaned/Baron_human3/Baron_human3_expr.csv",
+                  "/raid6/Kevin/singlecell_data/Cleaned/Baron_human4/Baron_human4_expr.csv",
+                  "/raid6/Kevin/singlecell_data/Cleaned/Baron_mouse1/Baron_mouse1_expr.csv",
+                  "/raid6/Kevin/singlecell_data/Cleaned/Baron_mouse2/Baron_mouse2_expr.csv",
+                  "/raid6/Kevin/singlecell_data/Cleaned/Darmanis/Darmanis_expr.csv")
+
+ncores <- 10
+doMC::registerDoMC(cores = ncores)
+
 # preprocessing_list <- vector("list", length(dat_file_vec))
 # for(i in 1:length(labels_file_vec)){
 #   labels <- read.csv(labels_file_vec[i])
