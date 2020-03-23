5f264dd4cd54d1fcad2d05d2dd7603324b0c57d8
Kevin Lin <klin1234@gmail.com>
2020-03-23 00:39:35 -0400

    Fix typo

--- main/step4_factorization.R
+++ main/step4_factorization.R
@@ -21,7 +21,7 @@ init <- eSVD::initialization(dat_impute, family = "curved_gaussian", k = k, max_
 esvd_embedding <- eSVD::fit_factorization(dat_impute, u_mat = init$u_mat, v_mat = init$v_mat,
                                                  family = "curved_gaussian",  reparameterize = T,
                                                  max_iter = 100, max_val = max_val,
-                                                 scalar = scalar_val,
+                                                 scalar = scalar,
                                                  return_path = F, cores = ncores,
                                                  verbose = T)
 
