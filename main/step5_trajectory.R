ba9022307a60e7f0d4dc8619b5574ebb3a67fbe7
Kevin Lin <klin1234@gmail.com>
2020-04-05 13:17:08 -0400

    Fixed typo

--- main/step5_trajectory.R
+++ main/step5_trajectory.R
@@ -8,17 +8,13 @@ cluster_group_list <- lapply(order_vec, function(x){
   grep(paste0("^", x), levels(cell_type_vec))
 })
 
-upscale_vec <- rep(NA, length(unique(cluster_labels)))
-size_vec <- sapply(cluster_group_list, function(x){length(which(cluster_labels %in% x))})
-for(i in 1:length(cluster_group_list)){
-  upscale_vec[cluster_group_list[[i]]] <- (max(size_vec)/size_vec[i])^(1/2)
-}
+upscale_factor <- 0.5
 
 p <- 3
 set.seed(10)
 esvd_curves <- eSVD::slingshot(esvd_embedding$u_mat[,1:p], cluster_labels, starting_cluster = cluster_group_list[[1]][1],
-                                    cluster_group_list = cluster_group_list,
-                                    verbose = T, upscale_vec = upscale_vec, reduction_percentage = 0.25)
+                               cluster_group_list = cluster_group_list,
+                               verbose = T, upscale_factor = upscale_factor, reduction_percentage = 0.25)
 
 print(paste0(Sys.time(), ": Finished eSVD trajectory"))
 save.image(paste0("../results/step5_trajectory", suffix, ".RData"))
@@ -26,7 +22,7 @@ save.image(paste0("../results/step5_trajectory", suffix, ".RData"))
 set.seed(10)
 esvd_bootstrap_list <- eSVD::bootstrap_curves(esvd_embedding$u_mat[,1:p], cluster_labels, starting_cluster = cluster_group_list[[1]][1],
                                              cluster_group_list = cluster_group_list, trials = 100,
-                                             upscale_vec = upscale_vec, reduction_percentage = 0.25, cores = ncores,
+                                             upscale_factor = upscale_factor, reduction_percentage = 0.25, cores = ncores,
                                              verbose = T)
 
 print(paste0(Sys.time(), ": Finished eSVD bootstrap"))
@@ -41,7 +37,7 @@ save.image(paste0("../results/step5_trajectory", suffix, ".RData"))
 
 svd_curves <- slingshot(svd_embedding[,1:p], cluster_labels, starting_cluster = cluster_group_list[[1]][1],
                         cluster_group_list = cluster_group_list,
-                        verbose = T, upscale_vec = upscale_vec, reduction_percentage = 0.25)
+                        verbose = T, upscale_factor = upscale_factor, reduction_percentage = 0.25)
 
 print(paste0(Sys.time(), ": Finished SVD trajectory"))
 save.image(paste0("../results/step5_trajectory", suffix, ".RData"))
@@ -49,7 +45,7 @@ save.image(paste0("../results/step5_trajectory", suffix, ".RData"))
 set.seed(10)
 svd_bootstrap_list <- eSVD::bootstrap_curves(svd_embedding[,1:p], cluster_labels, starting_cluster = cluster_group_list[[1]][1],
                                                    cluster_group_list = cluster_group_list, trials = 100,
-                                                   upscale_vec = upscale_vec, reduction_percentage = 0.25, cores = ncores,
+                                             upscale_factor = upscale_factor, reduction_percentage = 0.25, cores = ncores,
                                                verbose = T)
 
 
