3e9b3212d11c229a7b55c198f5030fb3c5504f7c
Kevin Lin <klin1234@gmail.com>
2020-05-08 08:21:35 -0400

    Fixed typo

--- main_supplement/main_baron.R
+++ main_supplement/main_baron.R
@@ -9,6 +9,6 @@ suffix <- "_spca_descend"
 ncores <- 10
 doMC::registerDoMC(cores = ncores)
 
-# source("../main_supplement/step0_baron_preprocessing.R")
-# source("../main_supplement/step1_baron_gaussian_fitting.R")
+source("../main_supplement/step0_baron_preprocessing.R")
+source("../main_supplement/step1_baron_gaussian_fitting.R")
 source("../main_supplement/step2_baron_scalar_tuning.R")
