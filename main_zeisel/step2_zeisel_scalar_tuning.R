748836ea553cd29eeb932a3380f7d0a4242cd6f4
Kevin Lin <klin1234@gmail.com>
2020-05-20 01:05:25 -0400

    Starting larger zeisel analysis

--- main_zeisel/step2_zeisel_scalar_tuning.R
+++ main_zeisel/step2_zeisel_scalar_tuning.R
@@ -1,12 +1,12 @@
 set.seed(10)
-load(paste0("../results/step1_baron_gaussian_fitting", suffix, ".RData"))
+load(paste0("../results/step1_zeisel_gaussian_fitting", suffix, ".RData"))
 session_info <- sessionInfo(); date_of_run <- Sys.time()
 
 neg_binom_vec <- c(250, 1000, 1e4, 1e6)
 curved_gaussian_vec <- c(1, 2, 4, 100)
 
 paramMat_esvd <- rbind(as.matrix(expand.grid(2, k_vec, neg_binom_vec, 50, 3000)),
-                  as.matrix(expand.grid(3, k_vec, curved_gaussian_vec, 50, 3000)))
+                       as.matrix(expand.grid(3, k_vec, curved_gaussian_vec, 50, 3000)))
 colnames(paramMat_esvd) <- c("fitting_distr", "k", "scalar", "max_iter", "max_val")
 
 esvd_missing_list_list <- vector("list", length(preprocessing_list))
@@ -38,22 +38,18 @@ fitting_func <- function(dat_impute, vec, missing_idx_list){
   tmp_list
 }
 
-for(i in 1:length(preprocessing_list)){
-  print(paste0(Sys.time(), ": Starting dataset ", i))
-  tmp <- vector("list", nrow(paramMat_esvd))
+esvd_missing_list <- vector("list", nrow(paramMat_esvd))
 
-  for(ii in 1:nrow(paramMat_esvd)){
-    print(paste0("Starting parameter row ", ii))
-    tmp[[ii]] <- fitting_func(dat_impute = preprocessing_list[[i]]$dat_impute, vec = paramMat_esvd[ii,],
-                              missing_idx_list = missing_idx_list_list[[i]])
-  }
+for(ii in 1:nrow(paramMat_esvd)){
+  print(paste0("Starting parameter row ", ii))
+  esvd_missing_list[[ii]] <- fitting_func(dat_impute = dat, vec = paramMat_esvd[ii,],
+                            missing_idx_list = missing_idx_list)
 
-  esvd_missing_list_list[[i]] <- tmp
-  save.image(paste0("../results/step2_baron_scalar_tuning", suffix, ".RData"))
+  save.image(paste0("../results/step2_zeisel_scalar_tuning", suffix, ".RData"))
 }
 
 print(paste0(Sys.time(), ": Finished scalar tuning for eSVD"))
-source_code_info <- c(source_code_info, readLines("../main_supplement/step2_baron_scalar_tuning.R"))
-save.image(paste0("../results/step2_baron_scalar_tuning", suffix, ".RData"))
+source_code_info <- c(source_code_info, readLines("../main_zeisel/step2_zeisel_scalar_tuning.R"))
+save.image(paste0("../results/step2_zeisel_scalar_tuning", suffix, ".RData"))
 print(warnings())
 
