cb4c380548fd8fc3f811720ae95e80c4fc3f80e2
Kevin Lin <klin1234@gmail.com>
2020-05-20 01:06:25 -0400

    Fixed typo

--- main_zeisel/main_zeisel.R
+++ main_zeisel/main_zeisel.R
@@ -11,4 +11,5 @@ session_info <- sessionInfo(); date_of_run <- Sys.time()
 source_code_info <- ""
 
 # source("../main_zeisel/step0_zeisel_preprocessing.R")
-source("../main_zeisel/step1_zeisel_gaussian_fitting.R")
+# source("../main_zeisel/step1_zeisel_gaussian_fitting.R")
+source("../main_zeisel/step2_zeisel_scalar_tuning.R")
