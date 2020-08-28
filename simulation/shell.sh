f9239e0bb804f710e71db69749cbafd047678778
Kevin Lin <klin1234@gmail.com>
2020-08-28 01:13:49 -0400

    Adding a shell script to make my life easier

---
+++ simulation/shell.sh
@@ -0,0 +1,4 @@
+nohup R-3.6.1 CMD BATCH factorization_suite_curved_gaussian_rest.R
+nohup R-3.6.1 CMD BATCH factorization_suite_negbinom_rest.R
+nohup R-3.6.1 CMD BATCH factorization_suite_poisson_rest.R
+nohup R-3.6.1 CMD BATCH factorization_suite_zinbwave_rest.R
