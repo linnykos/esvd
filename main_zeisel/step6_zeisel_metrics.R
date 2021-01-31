c94dfdd34df26c0f16dcedfe455ac39d6fb30b4b
Kevin Lin <klin1234@gmail.com>
2021-01-31 12:47:56 -0500

    Updating the latest Zeisel analysis

--- main_zeisel/step6_zeisel_metrics.R
+++ main_zeisel/step6_zeisel_metrics.R
@@ -1,88 +1,55 @@
+rm(list=ls())
+load("../results/step5_zeisel_comparison.RData")
+cluster_labels <- as.numeric(label_vec);
+levels(label_vec)
+color_func <- function(alpha = 0.2){
+  c(grDevices::rgb(240/255, 228/255, 66/255, alpha), #yellow
+    grDevices::rgb(86/255, 180/255, 233/255, alpha), #skyblue
+    grDevices::rgb(0/255, 158/255, 115/255, alpha), #bluish green
+    grDevices::rgb(0/255, 114/255, 178/255,alpha), #blue
+    grDevices::rgb(230/255, 159/255, 0/255,alpha), #orange
+    grDevices::rgb(100/255, 100/255, 100/255, alpha), #gray
+    grDevices::rgb(238/255, 31/255, 239/255, alpha))  #magenta
+}
+col_vec <- color_func(1)
 
-neigh_size <- determine_minimium_neighborhood_size(esvd_embedding$u_mat)
-set.seed(10)
-res <- compute_purity(esvd_embedding$u_mat, cluster_labels,
-                      neighborhood_size = neigh_size, num_samples = 5000)
+method_vec <- c("eSVD", "SVD", "ICA", "Diffusion map", "NMF", "Isomap", "ZINB-WaVE",
+                "pCMF")
+fit_list <- list(esvd_fit, svd_embedding, ica_fit, diffusion_fit,
+                 nnmf_fit, isomap_fit, zinbwave_fit, pcmf_fit)
 
-vec <- res$value_list[[1]]
-mean(vec)
-mean(sort(vec)[1:round(length(vec)/4)])
-quantile(sort(vec)[1:round(length(vec)/4)], probs = c(0, 0.25, 0.5, 0.75, 1))
+umap_list <- vector("list", 8)
+for(i in 1:length(fit_list)){
+  set.seed(10); umap_list[[i]] <- Seurat::RunUMAP(fit_list[[i]])@cell.embeddings
+}
+
+order_vec <- c(1,7,8,2,5,3,6,4)
+umap_list <- umap_list[order_vec]; method_vec <- method_vec[order_vec]
 
 # try it our embedding
-set.seed(10)
-zz <- Seurat::RunUMAP(esvd_embedding$u_mat)@cell.embeddings
-plot(zz[,1], zz[,2], col = cluster_labels, pch = 16, asp = T)
+grDevices::png(filename = paste0("../../esvd_results/figure/main/zeisel_comparison.png"),
+               height = 2200, width = 2000, res = 300,
+               units = "px")
+par(mfrow = c(3,3), mar = c(4,4,2,0.5))
+for(i in 1:8){
+  graphics::plot(umap_list[[i]][,1], umap_list[[i]][,2],
+       col = col_vec[cluster_labels], pch = 16, asp = T,
+       main = "", xlab = "UMAP dim. 1", ylab = "UMAP dim. 2")
+  graphics::title(method_vec[i], line = 0.3)
+}
+graphics.off()
 
-########
+################################################
 
-# SVD
-zz <- RSpectra::svds(dat, k = 20)
-tmp <- zz$u %*% diag(sqrt(zz$d))
-set.seed(10)
-zz <- Seurat::RunUMAP(tmp)@cell.embeddings
-plot(zz[,1], zz[,2], col = cluster_labels, pch = 16, asp = T)
+purity_list <- vector("list", 8)
+neigh_size_vec <- rep(NA, 8)
+for(i in 1:8){
+  print(i)
+  neigh_size_vec[i] <- determine_minimium_neighborhood_size(fit_list[[i]])
+  set.seed(10)
+  purity_list[[i]] <- unlist(compute_purity(fit_list[[i]], cluster_labels,
+                        neighborhood_size = neigh_size_vec[i], num_samples = 5000)$value_list)
+}
 
-#####
-
-
-
-set.seed(10)
-zz <- Seurat::RunUMAP(fit)@cell.embeddings
-plot(zz[,1], zz[,2], col = cluster_labels, pch = 16, asp = T)
-
-neigh_size <- determine_minimium_neighborhood_size(fit)
-set.seed(10)
-res <- compute_purity(fit, cluster_labels,
-                      neighborhood_size = neigh_size, num_samples = 5000)
-vec <- res$value_list[[1]]
-mean(vec)
-mean(sort(vec)[1:round(length(vec)/4)])
-quantile(sort(vec)[1:round(length(vec)/4)], probs = c(0, 0.25, 0.5, 0.75, 1))
-
-####
-
-
-set.seed(10)
-zz <- Seurat::RunUMAP(fit)@cell.embeddings
-plot(zz[,1], zz[,2], col = cluster_labels, pch = 16, asp = T)
-
-neigh_size <- determine_minimium_neighborhood_size(fit)
-set.seed(10)
-res <- compute_purity(fit, cluster_labels,
-                      neighborhood_size = neigh_size, num_samples = 5000)
-vec <- res$value_list[[1]]
-mean(vec)
-mean(sort(vec)[1:round(length(vec)/4)])
-quantile(sort(vec)[1:round(length(vec)/4)], probs = c(0, 0.25, 0.5, 0.75, 1))
-
-################
-
-
-
-neigh_size <- eSVD::determine_minimium_neighborhood_size(fit)
-set.seed(10)
-res <- eSVD::compute_purity(fit, cluster_labels,
-                            neighborhood_size = neigh_size, num_samples = 5000)
-vec <- res$value_list[[1]]
-mean(vec)
-mean(sort(vec)[1:round(length(vec)/4)])
-quantile(sort(vec)[1:round(length(vec)/4)], probs = c(0, 0.25, 0.5, 0.75, 1))
-
-##################
-
-
-neigh_size <- eSVD::determine_minimium_neighborhood_size(fit)
-set.seed(10)
-res <- eSVD::compute_purity(fit, cluster_labels,
-                            neighborhood_size = neigh_size, num_samples = 5000)
-vec <- res$value_list[[1]]
-mean(vec)
-mean(sort(vec)[1:round(length(vec)/4)])
-quantile(sort(vec)[1:round(length(vec)/4)], probs = c(0, 0.25, 0.5, 0.75, 1))
-
-
-####################
-
-
+sapply(purity_list[order_vec], function(x){quantile(x, probs = c(0.05,0.1,0.15))})
 
